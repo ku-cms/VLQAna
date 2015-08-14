bfe4fed6807efa0c931489fa5045d5a98c7d4bed
Devdatta <devdatta.majumder@cern.ch>
2015-08-14 14:13:51 +0200

    updating path for Eventounter

--- test/os2lana_cfg.py
+++ test/os2lana_cfg.py
@@ -37,9 +37,8 @@ from infiles_cfi import *
 process.source = cms.Source(
     "PoolSource",
     fileNames = cms.untracked.vstring(
-      #'file:/afs/cern.ch/user/d/devdatta/afswork/public/B2GEDMNtuple_SingleMuon.root'
-      #'file:/afs/cern.ch/user/d/devdatta/afswork/public/B2GEDMNtuple_WJets.root'
-     '/store/user/oiorio/ttDM/samples/July2015/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/EDMNTUPLE_28July/150727_234037/0000/B2GEDMNtuple_1.root' 
+     '/store/user/oiorio/ttDM/samples/Aug2015/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/EDMNTUPLE_10Aug/150810_160018/0000/B2GEDMNtuple_1.root',     
+     #'/store/user/decosa/ttDM/CMSSW_74X_V6/SingleMuon/SingleMuon_Run2015B-PromptReco-v74x_V5_1/150810_135051/0000/B2GEDMNtuple_1.root'
       )
       )
 
@@ -68,7 +67,7 @@ process.out = cms.OutputModule("PoolOutputModule",
     )
 
 ## Event counter
-from MyAnalysis.EventCounter.eventcounter_cfi import eventCounter
+from Analysis.EventCounter.eventcounter_cfi import eventCounter
 process.allEvents = eventCounter.clone()
 process.selectedEvents = eventCounter.clone()
 
