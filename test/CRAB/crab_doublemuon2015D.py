c531faed673f93feef130386585e456d0f18efff
Devdatta <devdatta.majumder@cern.ch>
2015-10-12 23:51:02 +0200

    crab cfg for data: to be modified when new ntuples are made

--- test/CRAB/crab_doublemuon2015D.py
+++ test/CRAB/crab_doublemuon2015D.py
@@ -2,8 +2,8 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'DoubleMuon2015D-PromptReco-v3_Sep26_v74x_V7_25ns'
-config.General.workArea = 'OS2LAna_13oct2015/'
+config.General.requestName = ''
+config.General.workArea = 'OS2LAna_14Oct2015/'
 config.General.transferLogs = True
 
 config.section_("JobType")
@@ -12,14 +12,14 @@ config.JobType.psetName = 'os2lana_cfg.py'
 config.JobType.pyCfgParams = ['isData=True', 'zdecaymode=zmumu']
 
 config.section_("Data")
-config.Data.inputDataset = '/DoubleMuon/devdatta-crab_Run2015D-PromptReco-v3_Sep26_v74x_V7_25ns-8a6e833b8257c2f318a8d19495a1b105/USER'
+config.Data.inputDataset = ''
 config.Data.inputDBS = 'phys03'
-config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-257599_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-258159_13TeV_PromptReco_Collisions15_25ns_JSON_v3.txt'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 20
 config.Data.ignoreLocality = True
 config.Data.publication = False
-config.Data.outLFNDirBase = '/store/user/devdatta/OS2LAna_13oct2015/'
+config.Data.outLFNDirBase = '/store/user/devdatta/OS2LAna_14Oct2015/'
 # This string is used to construct the output dataset name
 
 config.section_("Site")
