3f9c61c6b5e9d4bbf3b21fcf7988919bd6d5535c
Devdatta <devdatta.majumder@cern.ch>
2015-10-29 20:08:37 +0100

    Updating DY samples

--- test/CRAB/crab_DY50_HT_600toInf.py
+++ test/CRAB/crab_DY50_HT_600toInf.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'DYamcnlo_25ns'
+config.General.requestName = 'DY_HT600toInf_25ns'
 config.General.workArea = 'OS2LAna_29Oct2015/'
 config.General.transferLogs = True
 
@@ -13,10 +13,10 @@ config.JobType.pyCfgParams = ['isData=False', 'doPUReweightingOfficial=True']
 config.JobType.inputFiles = ['hnpv_data_Run2015D_mc_RunIISpring15DR74-Asympt25ns_pvwt.root', 'PUDistData_Run2015ABCD.root', 'PUDistMC_2015_25ns_Startup_PoissonOOTPU.root']
 
 config.section_("Data")
-config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/phys_b2g-B2GAnaFW_v74x_v6p1_25ns-V2-d5e3976e154b1b4043d031aaa9c2809b/USER'
+config.Data.inputDataset = '/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 3
+config.Data.unitsPerJob = 5
 config.Data.ignoreLocality = False
 config.Data.publication = False
 config.Data.outLFNDirBase = '/store/user/devdatta/OS2LAna_29Oct2015/'
