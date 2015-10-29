fd4e8fe233df6d9caf1253ab7c532e38b1f8e0d5
Devdatta <devdatta.majumder@cern.ch>
2015-10-29 20:15:22 +0100

    Updating DY samples

--- test/CRAB/crab_DY50_HT_600toInf.py
+++ test/CRAB/crab_DY50_HT_600toInf.py
@@ -13,7 +13,7 @@ config.JobType.pyCfgParams = ['isData=False', 'doPUReweightingOfficial=True']
 config.JobType.inputFiles = ['hnpv_data_Run2015D_mc_RunIISpring15DR74-Asympt25ns_pvwt.root', 'PUDistData_Run2015ABCD.root', 'PUDistMC_2015_25ns_Startup_PoissonOOTPU.root']
 
 config.section_("Data")
-config.Data.inputDataset = '/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM'
+config.Data.inputDataset = '/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/vorobiev-B2GAnaFW_Run2Spring15_25ns_v74x_V61-d5e3976e154b1b4043d031aaa9c2809b/USER'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 5
