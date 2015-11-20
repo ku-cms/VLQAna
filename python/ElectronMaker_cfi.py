f8d5002c360345fb42b232b1fb9c2423d35dd0d6
Devdatta <devdatta.majumder@cern.ch>
2015-11-20 15:22:51 +0100

    looser electron and muon iso

--- python/ElectronMaker_cfi.py
+++ python/ElectronMaker_cfi.py
@@ -5,10 +5,10 @@ from Analysis.VLQAna.Electron_cfi import *
 defaultElectronMakerParameters = cms.PSet(
     defaultElectronParameters, 
     elidtype = cms.string("LOOSE"), #Using electron ID coded in VID tool
-    elPtMin = cms.double(40),
+    elPtMin = cms.double(25),
     elPtMax = cms.double(10000),
     elAbsEtaMax = cms.double(2.4),
     elIsoMin = cms.double(0.00),
-    elIsoMax = cms.double(0.1), # No need to set electron isolation by hand
+    elIsoMax = cms.double(0.2), # No need to set electron isolation by hand
     useVID = cms.bool(True), 
     )
