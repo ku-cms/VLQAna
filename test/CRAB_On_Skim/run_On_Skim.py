94dc776ea46c5500ca9396f45457d21bb44552dc
skhalil <skhalil@fnal.gov>
2015-12-08 13:15:03 -0600

    work in progress: automate carb submisisons

--- test/CRAB_On_Skim/run_On_Skim.py
+++ test/CRAB_On_Skim/run_On_Skim.py
@@ -18,6 +18,11 @@ parser.add_option('--outLabel', metavar='L', type='string', action='store',
                   dest='outLabel',
                   help='output tag to be used')
 
+parser.add_option('--skimType', metavar='S', type='string', action='store',
+                  default=' CR_Zelel',
+                  dest='skimType',
+                  help='Skim type: CR_Zelel, CR_Zmumu, SR_Zelel, SR_Zmumu')
+    
 parser.add_option('--isData', metavar='I', type='int', action='store',
                   default=0,
                   dest='isData',
@@ -50,39 +55,25 @@ for job in jobList:
      a1 = a0.replace( 'POSTFIX', options.outLabel)
      a2 = a1.replace( 'DUMMY_DATASET', job[0] )
      a3 = a2.replace( 'DATA', isData)
-     print a3
-   
-    #with open("crab_dummy_os2lana.py") as f:
-    #    old = f.read() 
-    #    old.replace('DUMMY_NAME', job[0])
-    #    print old
-#now write new one:
-#with open("test.py", "w") as f:
-#    f.write(file_str)
+     a4 = a3.replace( 'DUMMY_SITE','T3_US_FNALLPC')
+     a5 = a4.replace( 'DUMMY_OUTPUT_PATH', '/store/group/lpcbprime/noreplica/skhalil/Histo/'+options.skimType) 
 
-
-
-
-#result = src.substitute(d)
-
-#print d
-
-#import subprocess, os, ConfigParser
-
-
-#dummy_config_name = 'crab_dummy_os2lana.py'
-
-#dummy_config = ConfigParser.RawConfigParser()
-#dummy_config.read(dummy_config_name)
-
-#pset = 'os2lana_cfg.py'
-
-# one pset for all jobs
-#dummy_config.set('CMSSW','pset',pset)
-
-#joblist = [
-#     {DUMMY_NAME:'DY_HT100to200', DUMMY_DATASET:'/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/skhalil-crab_DY_HT100to200_25ns_CR_Zelel-7ad39afaa9141e6c9414a79f4df4bdce/USER'}, 
-#]
-
-#for job in joblist:
-#    config = dummy_config
+     # Dump the contents of the crab config to the screen
+     print '------ Config : ------- '
+     print a5
+     
+     # open the output file
+     crabName = 'crab_' + outname + '.py'
+     fout = open( crabName, 'w')
+    # write the text to the output file
+     fout.write( a5 )
+     fout.close()
+     print '------ CRAB starting up! ------'
+    # now create the job:
+     s = 'crab -create -c ' + crabName
+     print s
+    # and submit:
+    # subprocess.call( [s], shell=True )
+    #s = 'crab -submit -c ' + outname
+    #print s
+    #subprocess.call( [s], shell=True )
