e0e149ff38250598c33e4193f25c61a8aa2ec0f7
skhalil <skhalil@fnal.gov>
2015-12-09 03:22:10 -0600

    fixed the auto crab submission script

--- test/CRAB_On_Skim/crab_dummy_os2lana.py
+++ test/CRAB_On_Skim/crab_dummy_os2lana.py
@@ -4,13 +4,13 @@ config = Configuration()
 config.section_("General")
 
 config.General.requestName = DUMMY_NAME
-config.General.workArea = POSTFIX
+config.General.workArea = DUMMY_WORKDIR
 config.General.transferLogs = True
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'os2lana_cfg.py'
-config.JobType.pyCfgParams = [DATA]
+config.JobType.pyCfgParams = [DATA, MODE, PUOFF, FILTERSIGNAL]
 config.JobType.inputFiles = ['Summer15_25nsV6_MC_L2L3Residual_AK8PFchs.txt', 'Summer15_25nsV6_MC_L3Absolute_AK8PFchs.txt', 'hnpv_data_Run2015D_mc_RunIISpring15DR74-Asympt25ns_pvwt.root', 'PUDistData_Run2015ABCD.root', 'PUDistMC_2015_25ns_Startup_PoissonOOTPU.root']
 
 config.section_("Data")
--- test/CRAB_On_Skim/run_On_Skim.py
+++ test/CRAB_On_Skim/run_On_Skim.py
@@ -1,5 +1,5 @@
 #! /bin/python
-
+import os
 from string import *
 import subprocess
 
@@ -19,7 +19,7 @@ parser.add_option('--outLabel', metavar='L', type='string', action='store',
                   help='output tag to be used')
 
 parser.add_option('--skimType', metavar='S', type='string', action='store',
-                  default=' CR_Zelel',
+                  default='CR_Zelel',
                   dest='skimType',
                   help='Skim type: CR_Zelel, CR_Zmumu, SR_Zelel, SR_Zmumu')
     
@@ -32,11 +32,20 @@ parser.add_option('--isData', metavar='I', type='int', action='store',
 
 (options, args) = parser.parse_args()
 
+#set the parameter options in os2lana.py
 if options.isData == 1:
     isData  = 'isData=True'
+    puOfficial = 'doPUReweightingOfficial=False'
 else: 
     isData = 'isData=False'
+    puOfficial = 'doPUReweightingOfficial=True'
 
+if 'Zmumu' in options.skimType:
+    mode = 'zdecaymode=zmumu'
+elif 'Zelel' in options.skimType:
+    mode = 'zdecaymode=zelel'
+else: 
+    mode = 'zdecaymode= '
 
 jobList = [
      ['/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/skhalil-crab_DY_HT100to200_25ns_CR_Zelel-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'DY_HT100to200'],
@@ -44,33 +53,42 @@ jobList = [
      ]
 
 for job in jobList:
-     print '------------------------- prepare to run on :  ' + job[0] + ' ---------------------------'
+     print '------prepare to run on :  ' + job[0] + ' -----------'
      f = open(options.inputCfg, 'r')
      instring = f.read()
      
-    # Get the dataset name, without the user-level stuff
      baseList = job[0].split('/')
-     outname = baseList[1] + '_' + options.outLabel
-     a0 = instring.replace( 'DUMMY_NAME', outname )
-     a1 = a0.replace( 'POSTFIX', options.outLabel)
-     a2 = a1.replace( 'DUMMY_DATASET', job[0] )
-     a3 = a2.replace( 'DATA', isData)
-     a4 = a3.replace( 'DUMMY_SITE','T3_US_FNALLPC')
-     a5 = a4.replace( 'DUMMY_OUTPUT_PATH', '/store/group/lpcbprime/noreplica/skhalil/Histo/'+options.skimType) 
+     if 'Tprime' in baseList[0] or 'Bprime' in baseList[0]: 
+         filterSignal = 'filterSignal=True'
+     else: filterSignal = 'filterSignal=False'
+
+     outname = job[1] + '_' + options.outLabel
+     a0 = instring.replace( 'DUMMY_NAME', "'"+outname+"'" )
+     a1 = a0.replace( 'DUMMY_WORKDIR', "'"+options.outLabel+"'")
+     a2 = a1.replace( 'DUMMY_DATASET', "'"+job[0]+"'" )
+     a3 = a2.replace( 'DATA', "'"+isData+"'") 
+     a4 = a3.replace( 'MODE', "'"+mode+"'")
+     a5 = a4.replace( 'PUOFF', "'"+puOfficial+"'")
+     a6 = a5.replace( 'FILTERSIGNAL', "'"+filterSignal+"'")
+     a7 = a6.replace( 'DUMMY_SITE',"'"+'T3_US_FNALLPC'+"'")
+     a8 = a7.replace( 'DUMMY_OUTPUT_PATH', "'"+'/store/group/lpcbprime/noreplica/skhalil/Histo/'+options.skimType+"'") 
 
      # Dump the contents of the crab config to the screen
      print '------ Config : ------- '
-     print a5
-     
+     print a8
+     #create a directory if it doesn't exist
+     c = 'mkdir '+options.skimType
+     if not os.path.isdir(options.skimType):
+         subprocess.call( [c], shell=True )
      # open the output file
      crabName = 'crab_' + outname + '.py'
-     fout = open( crabName, 'w')
+     fout = open( options.skimType+'/'+crabName, 'w')
     # write the text to the output file
-     fout.write( a5 )
+     fout.write( a8 )
      fout.close()
      print '------ CRAB starting up! ------'
     # now submit the job:
-     s = 'crab submit -c ' + crabName
+     s = 'crab submit -c ' + options.skimType+'/'+crabName
      print s
     # and submit:
     # subprocess.call( [s], shell=True )
