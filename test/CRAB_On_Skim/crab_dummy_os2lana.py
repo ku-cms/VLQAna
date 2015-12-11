4f7126fe1b9bfefdd6076c847041adeb4ca8f8a1
skhalil <skhalil@fnal.gov>
2015-12-11 02:42:09 -0600

    extending the datasets names options

--- test/CRAB_On_Skim/crab_dummy_os2lana.py
+++ test/CRAB_On_Skim/crab_dummy_os2lana.py
@@ -10,7 +10,7 @@ config.General.transferLogs = True
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'os2lana_cfg.py'
-config.JobType.pyCfgParams = [DATA, MODE, PUOFF, FILTERSIGNAL]
+config.JobType.pyCfgParams = [DATA, MODE, PUOFF, LEPSF, FILTERSIGNAL, SIGNALTYPE]
 config.JobType.inputFiles = ['Summer15_25nsV6_MC_L2L3Residual_AK8PFchs.txt', 'Summer15_25nsV6_MC_L3Absolute_AK8PFchs.txt', 'hnpv_data_Run2015D_mc_RunIISpring15DR74-Asympt25ns_pvwt.root', 'PUDistData_Run2015ABCD.root', 'PUDistMC_2015_25ns_Startup_PoissonOOTPU.root']
 
 config.section_("Data")
--- test/CRAB_On_Skim/run_On_Skim.py
+++ test/CRAB_On_Skim/run_On_Skim.py
@@ -19,7 +19,7 @@ parser.add_option('--outLabel', metavar='L', type='string', action='store',
                   help='output tag to be used')
 
 parser.add_option('--skimType', metavar='S', type='string', action='store',
-                  default='CR_Zelel',
+                  default='CR_Zmumu',
                   dest='skimType',
                   help='Skim type: CR_Zelel, CR_Zmumu, SR_Zelel, SR_Zmumu')
     
@@ -42,40 +42,76 @@ else:
 
 if 'Zmumu' in options.skimType:
     mode = 'zdecaymode=zmumu'
+    applySF = 'applyLeptonSFs=False'
 elif 'Zelel' in options.skimType:
     mode = 'zdecaymode=zelel'
-else: 
-    mode = 'zdecaymode= '
+    applySF = 'applyLeptonSFs=True'
 
-jobList = [
-     ['/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/skhalil-crab_DY_HT100to200_25ns_CR_Zelel-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'DY_HT100to200'],
-     
-     ]
+# add all the datasets for particular skim
+if options.skimType=='CR_Zelel':
+    print 'Submitting jobs for electron channel in control region -------->'
+    jobList = [
+        ['/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/skhalil-crab_DY_HT100to200_25ns_CR_Zelel-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'DY_HT100to200', ''],
+        ['/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/skhalil-crab_DY_HT200to400_25ns_CR_Zelel-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'DY_HT200to400', ''],
+        ['/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/skhalil-crab_DY_HT400to600_25ns_CR_Zelel-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'DY_HT400to600', ''],
+        ['/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/skhalil-crab_DY_HT600toInf_25ns_CR_Zelel-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'DY_HT600toInf', ''],
+        ['/TT_TuneCUETP8M1_13TeV-powheg-pythia8/skhalil-crab_TT_powheg-pythia8_ext3_25ns_CR_Zelel-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TT-powheg-pythia8-ext3', ''],
+        ['/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/skhalil-crab_TTJets_MG_pythia8_asymptotic_25ns_CR_Zelel-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TTJets_incl_madgraphMLM-pythia8', ''],
+        ['/TTJets_HT-600to800_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/skhalil-crab_TTJets_HT-600to800_25ns_CR_Zelel-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TTJets_HT-600to800_madgraphMLM-pythia8', ''],
+        ['/TTJets_HT-800to1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/skhalil-crab_TTJets_HT-800to1200_25ns_CR_Zelel-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TTJets_HT-800to1200_madgraphMLM-pythia8', ''],
+        ['/TTJets_HT-1200to2500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/skhalil-crab_TTJets_HT-1200to2500_25ns_CR_Zelel-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TTJets_HT-1200to2500_madgraphMLM-pythia8', ''],
+        ['/TTJets_HT-2500toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/skhalil-crab_TTJets_HT-2500toInf_25ns_CR_Zelel-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TTJets_HT-2500toInf_madgraphMLM-pythia8', ''],
+        ['/TprimeTprime_M-800_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-800_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZtZ_M-800', 'EvtType_MC_tZtZ'],
+        ['/TprimeTprime_M-800_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-800_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZtH_M-800', 'EvtType_MC_tZtH'],
+        ['/TprimeTprime_M-800_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-800_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZbW_M-800', 'EvtType_MC_tZbW'],
+        ['/TprimeTprime_M-900_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-900_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZtZ_M-900', 'EvtType_MC_tZtZ'],
+        ['/TprimeTprime_M-900_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-900_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZtH_M-900', 'EvtType_MC_tZtH'],
+        ['/TprimeTprime_M-900_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-900_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZbW_M-900', 'EvtType_MC_tZbW'],
+        ['/TprimeTprime_M-1000_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-1000_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZtZ_M-1000', 'EvtType_MC_tZtZ'],
+        ['/TprimeTprime_M-1000_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-1000_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZtH_M-1000', 'EvtType_MC_tZtH'],
+        ['/TprimeTprime_M-1000_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-1000_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZbW_M-1000', 'EvtType_MC_tZbW'],
+        ['/TprimeTprime_M-1200_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-1200_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZtZ_M-1200', 'EvtType_MC_tZtZ'],
+        ['/TprimeTprime_M-1200_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-1200_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZtH_M-1200', 'EvtType_MC_tZtH'],
+        ['/TprimeTprime_M-1200_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-1200_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZbW_M-1200', 'EvtType_MC_tZbW'],
+        ['/TprimeTprime_M-1500_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-1500_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZtZ_M-1500', 'EvtType_MC_tZtZ'],
+        ['/TprimeTprime_M-1500_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-1500_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZtH_M-1500', 'EvtType_MC_tZtH'],
+        ['/TprimeTprime_M-1500_TuneCUETP8M1_13TeV-madgraph-pythia8/skhalil-crab_TprimeTprime_M-1500_25ns-7ad39afaa9141e6c9414a79f4df4bdce/USER', 'TpTp_tZbW_M-1500', 'EvtType_MC_tZbW'],
+        ]
 
+if options.skimType=='CR_Zmumu':
+    print 'Submitting jobs for muon channel in control region -------->'
+    jobList = [
+        ['/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/skhalil-crab_DY_HT100to200_25ns_CR_Zmumu-30a01c6a40dfe28fc773f3b6cedc2f04/USER', 'DY_HT100to200', ''],
+        ]
+    
 for job in jobList:
      print '------prepare to run on :  ' + job[0] + ' -----------'
      f = open(options.inputCfg, 'r')
      instring = f.read()
-     
      baseList = job[0].split('/')
-     if 'Tprime' in baseList[0] or 'Bprime' in baseList[0]: 
-         filterSignal = 'filterSignal=True'
-     else: filterSignal = 'filterSignal=False'
-
      outname = job[1] + '_' + options.outLabel
-     a0 = instring.replace( 'DUMMY_NAME', "'"+outname+"'" )
-     a1 = a0.replace( 'DUMMY_WORKDIR', "'"+options.outLabel+"'")
-     a2 = a1.replace( 'DUMMY_DATASET', "'"+job[0]+"'" )
-     a3 = a2.replace( 'DATA', "'"+isData+"'") 
-     a4 = a3.replace( 'MODE', "'"+mode+"'")
+
+     a0 = instring.replace( 'DUMMY_WORKDIR', "'"+options.outLabel+"'")
+     a1 = a0.replace( 'DUMMY_DATASET', "'"+job[0]+"'" )
+     a2 = a1.replace( 'DATA', "'"+isData+"'") 
+     a3 = a2.replace( 'MODE', "'"+mode+"'")
+     a4 = a3.replace( 'LEPSF', "'"+applySF+"'")
      a5 = a4.replace( 'PUOFF', "'"+puOfficial+"'")
-     a6 = a5.replace( 'FILTERSIGNAL', "'"+filterSignal+"'")
-     a7 = a6.replace( 'DUMMY_SITE',"'"+'T3_US_FNALLPC'+"'")
-     a8 = a7.replace( 'DUMMY_OUTPUT_PATH', "'"+'/store/group/lpcbprime/noreplica/skhalil/Histo/'+options.skimType+"'") 
+    
+     if 'Tprime' in baseList[1] or 'Bprime' in baseList[1]:     
+         a6 = a5.replace( 'FILTERSIGNAL', "'filterSignal=True'")
+         a7 = a6.replace('SIGNALTYPE', "'signalType="+job[2]+"'")  
+     else:
+         a6 = a5.replace( ', FILTERSIGNAL', '')
+         a7 = a6.replace( ', SIGNALTYPE', '')
+         
+     a8 = a7.replace( 'DUMMY_NAME', "'"+outname+"'" )
+     a9 = a8.replace( 'DUMMY_SITE',"'"+'T3_US_FNALLPC'+"'")
+     a10 = a9.replace( 'DUMMY_OUTPUT_PATH', "'"+'/store/group/lpcbprime/noreplica/skhalil/Histo/'+options.skimType+"'") 
 
      # Dump the contents of the crab config to the screen
      print '------ Config : ------- '
-     print a8
+     print a10
      #create a directory if it doesn't exist
      c = 'mkdir '+options.skimType
      if not os.path.isdir(options.skimType):
@@ -83,13 +119,13 @@ for job in jobList:
      # open the output file
      crabName = 'crab_' + outname + '.py'
      fout = open( options.skimType+'/'+crabName, 'w')
-    # write the text to the output file
-     fout.write( a8 )
+     # write the text to the output file
+     fout.write( a10 )
      fout.close()
      print '------ CRAB starting up! ------'
-    # now submit the job:
+     # now submit the job:
      s = 'crab submit -c ' + options.skimType+'/'+crabName
      print s
-    # and submit:
-    # subprocess.call( [s], shell=True )
+     # and submit:
+     #subprocess.call( [s], shell=True )
     
