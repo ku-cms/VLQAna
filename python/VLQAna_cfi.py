54b90132385c5b622e15f4c928e7532453420c57
Devdatta <devdatta.majumder@cern.ch>
2016-02-14 16:25:00 +0100

    Adding isData vars

--- python/VLQAna_cfi.py
+++ python/VLQAna_cfi.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from Analysis.VLQAna.PickGenPart_cfi import *
 from Analysis.VLQAna.JetSelector_cfi import *
 ana = cms.EDFilter("VLQAna", 
+    isData                     = cms.InputTag("evtcleaner","isData"), 
+    hltdecision                = cms.InputTag("evtcleaner","hltdecision"), 
     evttype                    = cms.InputTag("evtcleaner","evttype"),
     evtwtGen                   = cms.InputTag("evtcleaner","evtwtGen"),
     evtwtPV                    = cms.InputTag("evtcleaner","evtwtPV"),
@@ -10,12 +12,13 @@ ana = cms.EDFilter("VLQAna",
     jetAK4selParams            = defaultAK4JetSelectionParameters,
     jetAK8selParams            = defaultAK8JetSelectionParameters,
     jetHTaggedselParams        = defaultHJetSelectionParameters,
-    jetAntiHTaggedselParams    = defaultAK8JetSelectionParameters.clone(
+    jetAntiHTaggedselParams    = defaultHJetSelectionParameters.clone(
+      subjetCSVMin = cms.double(-1000000) ,
       subjetCSVMax = cms.double(0.605) ,
       ), 
     jetTopTaggedselParams      = defaultTJetSelectionParameters.clone(
-      jettau3Bytau2Max    = cms.double(0.61) ,
-      subjetHighestCSVMin = cms.double(0.66) ,
+      jettau3Bytau2Max    = cms.double(0.54) ,
+      subjetHighestCSVMin = cms.double(0.79) ,
       ), 
     HTMin                      = cms.double  (1000.), 
     doBTagSFUnc                = cms.bool(False), 
