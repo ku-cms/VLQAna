5ba681adc0334ab5cc86df41a3852e57220490c3
Devdatta <devdatta.majumder@cern.ch>
2016-02-28 20:04:33 +0100

    adding event variable

--- test/hh4b_cfg.py
+++ test/hh4b_cfg.py
@@ -49,8 +49,10 @@ from inputFiles_cfi import *
 process.source = cms.Source(
     "PoolSource",
     fileNames = cms.untracked.vstring(
-    'root://cmsxrootd-site.fnal.gov//store/user/eschmitz/B2G/Spring15/ReMiniAOD/QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_retry-ReMini-v2-b2ganafw74xV8-2_QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151102_003543/0000/B2GEDMNtuple_73.root'
+    #'root://cmsxrootd-site.fnal.gov//store/user/eschmitz/B2G/Spring15/ReMiniAOD/QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_retry-ReMini-v2-b2ganafw74xV8-2_QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/151102_003543/0000/B2GEDMNtuple_73.root'
     #FileNames_BG1200
+    FileName_JetHT_PromptRecov4
+    #FileName_QCD_HT1000to1500
     #FileNames
     ) 
     )
@@ -66,6 +68,12 @@ process.evtcleaner.hltPaths = cms.vstring (hltpaths)
 process.evtcleaner.isData = options.isData 
 
 process.load('Analysis.VLQAna.HH4b_cff') 
+process.hh4b.jetAK8selParams.scaleJetP4 = cms.bool(False)
+process.hh4b.jetAK8selParams.jettau2Bytau1Max = cms.double(0.75)
+process.hh4b.jetAK8selParams.subjetCSVMin = cms.double(-1000.) 
+#process.hh4b.doBTagSFUnc = options.doBTagSFUnc
+process.hh4b.jetAK8selParams.jecShift = options.jecShift 
+process.hh4b.jetAK8selParams.jerShift = options.jerShift 
 process.hh4b.jetHTaggedselParams.scaleJetP4 = cms.bool(False)
 process.hh4b.jetHTaggedselParams.jettau2Bytau1Max = cms.double(0.75)
 process.hh4b.jetHTaggedselParams.subjetCSVMin = cms.double(-1000.) 
@@ -73,6 +81,9 @@ process.hh4b.jetHTaggedselParams.subjetCSVMin = cms.double(-1000.)
 process.hh4b.jetHTaggedselParams.jecShift = options.jecShift 
 process.hh4b.jetHTaggedselParams.jerShift = options.jerShift 
 if options.isData:
+  process.hh4b.jetAK8selParams.newJECPayloadNames.append("Summer15_25nsV7_DATA_L2L3Residual_AK8PFchs.txt")
+  process.hh4b.jetAK8selParams.jecUncPayloadName = cms.string("Summer15_25nsV7_DATA_Uncertainty_AK8PFchs.txt")
+  process.hh4b.jetAK8selParams.jecAK8GroomedPayloadNames.append("Summer15_25nsV7_DATA_L2L3Residual_AK8PFchs.txt") 
   process.hh4b.jetHTaggedselParams.newJECPayloadNames.append("Summer15_25nsV7_DATA_L2L3Residual_AK8PFchs.txt")
   process.hh4b.jetHTaggedselParams.jecUncPayloadName = cms.string("Summer15_25nsV7_DATA_Uncertainty_AK8PFchs.txt")
   process.hh4b.jetHTaggedselParams.jecAK8GroomedPayloadNames.append("Summer15_25nsV7_DATA_L2L3Residual_AK8PFchs.txt") 
