f6e618bd599f62a1e8fdc898d90402acb26659af
Devdatta <devdatta.majumder@cern.ch>
2016-03-08 11:59:47 +0100

    removing indexer template classes

--- plugins/VLQAna.cc
+++ plugins/VLQAna.cc
@@ -99,50 +99,6 @@ class VLQAna : public edm::EDFilter {
 
 using namespace std; 
 
-template <typename T>
-struct iterator_extractor { typedef typename T::iterator type; };
-
-template <typename T>
-struct iterator_extractor<T const> { typedef typename T::const_iterator type; };
-
-
-template <typename T>
-class Indexer {
-  public:
-    class iterator {
-      typedef typename iterator_extractor<T>::type inner_iterator;
-
-      typedef typename std::iterator_traits<inner_iterator>::reference inner_reference;
-      public:
-      typedef std::pair<size_t, inner_reference> reference;
-
-      iterator(inner_iterator it): _pos(0), _it(it) {}
-
-      reference operator*() const { return reference(_pos, *_it); }
-
-      iterator& operator++() { ++_pos; ++_it; return *this; }
-      iterator operator++(int) { iterator tmp(*this); ++*this; return tmp; }
-
-      bool operator==(iterator const& it) const { return _it == it._it; }
-      bool operator!=(iterator const& it) const { return !(*this == it); }
-
-      private:
-      size_t _pos;
-      inner_iterator _it;
-    };
-
-    Indexer(T& t): _container(t) {}
-
-    iterator begin() const { return iterator(_container.begin()); }
-    iterator end() const { return iterator(_container.end()); }
-
-  private:
-    T& _container;
-}; // class Indexer
-
-template <typename T>
-Indexer<T> index(T& t) { return Indexer<T>(t); }
-
 VLQAna::VLQAna(const edm::ParameterSet& iConfig) :
   l_isData                (iConfig.getParameter<edm::InputTag>     ("isData")),
   l_hltdecision           (iConfig.getParameter<edm::InputTag>     ("hltdecision")),
