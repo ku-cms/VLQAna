87e9504f35eb642175485202aabfe2d936b7e986
Devdatta <devdatta.majumder@cern.ch>
2016-03-08 11:59:07 +0100

    moving range-based for-loop indexer here

--- interface/Utilities.h
+++ interface/Utilities.h
@@ -5,6 +5,49 @@
 #include "AnalysisDataFormats/BoostedObjects/interface/Muon.h"
 #include "AnalysisDataFormats/BoostedObjects/interface/Jet.h"
 
+template <typename T>
+struct iterator_extractor { typedef typename T::iterator type; };
+
+template <typename T>
+struct iterator_extractor<T const> { typedef typename T::const_iterator type; };
+
+template <typename T>
+class Indexer {
+  public:
+    class iterator {
+      typedef typename iterator_extractor<T>::type inner_iterator;
+
+      typedef typename std::iterator_traits<inner_iterator>::reference inner_reference;
+      public:
+      typedef std::pair<size_t, inner_reference> reference;
+
+      iterator(inner_iterator it): _pos(0), _it(it) {}
+
+      reference operator*() const { return reference(_pos, *_it); }
+
+      iterator& operator++() { ++_pos; ++_it; return *this; }
+      iterator operator++(int) { iterator tmp(*this); ++*this; return tmp; }
+
+      bool operator==(iterator const& it) const { return _it == it._it; }
+      bool operator!=(iterator const& it) const { return !(*this == it); }
+
+      private:
+      size_t _pos;
+      inner_iterator _it;
+    };
+
+    Indexer(T& t): _container(t) {}
+
+    iterator begin() const { return iterator(_container.begin()); }
+    iterator end() const { return iterator(_container.end()); }
+
+  private:
+    T& _container;
+}; // class Indexer
+
+template <typename T>
+Indexer<T> index(T& t) { return Indexer<T>(t); }
+
 struct compareByPt {
   bool operator () (const vlq::Candidate& cand0, const vlq::Candidate& cand1) {
     return cand0.getPt() > cand1.getPt() ;     
