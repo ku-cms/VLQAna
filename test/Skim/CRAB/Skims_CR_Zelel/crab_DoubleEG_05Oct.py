141d4b9a45d31f5ad48e13a6b89c06983c4c9332
skhalil <skhalil@fnal.gov>
2016-03-11 16:30:41 -0600

    adding old changes

--- test/Skim/CRAB/Skims_CR_Zelel/crab_DoubleEG_05Oct.py
+++ test/Skim/CRAB/Skims_CR_Zelel/crab_DoubleEG_05Oct.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'DoubleEG_05Oct'
+config.General.requestName = 'DoubleEG_05Oct_CR_Zelel'
 config.General.workArea = 'B2GEDMNtuplesSkim_CR_Zelel_24Nov/'
 config.General.transferLogs = True
 
@@ -15,14 +15,15 @@ config.section_("Data")
 config.Data.inputDataset = '/DoubleEG/devdatta-Run2015D-05Oct2015-v1_B2GAnaFW_v74x_v8p4-ff3168b63d5cee365f49bf7ea3ba6ef3/USER'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'LumiBased'
-config.Data.unitsPerJob = 10
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
+config.Data.unitsPerJob = 20
 config.Data.ignoreLocality = False
 config.Data.publication = True
-config.Data.outLFNDirBase = '/store/group/phys_b2g/B2GAnaFW/Skims/CR_Zelel/'
-config.Data.outputDatasetTag = 'Run2015D-05Oct2015-v1_B2GAnaFW_v74x_v8p4_Skim_CR_Zelel_24Nov2015'
+config.Data.outLFNDirBase = '/store/group/lpcbprime/noreplica/skhalil/B2GEDMNtuplesSkim_CR_Zelel_20Nov/'
+config.Data.outputDatasetTag = 'Run2015D-05Oct2015-v1_B2GAnaFW_v74x_v8p4_Skim_CR_Zelel_06Jan2016'
 # This string is used to construct the output dataset name
 
 config.section_("Site")
-config.Site.storageSite = 'T2_CH_CERN'
+config.Site.storageSite = 'T3_US_FNALLPC'
 
 config.section_('User')
--- test/Skim/CRAB/Skims_CR_Zmumu/crab_DoubleMuon_PromptRecov4.py
+++ test/Skim/CRAB/Skims_CR_Zmumu/crab_DoubleMuon_PromptRecov4.py
@@ -2,8 +2,8 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'DoubleMuon_PromptRecov4'
-config.General.workArea = 'B2GEDMNtuplesSkim_CR_Zmumu_24Nov/'
+config.General.requestName = 'DoubleMuon_PromptRecov4_CR_Zmumu'
+config.General.workArea = 'B2GEDMNtuplesSkim_CR_Zmumu_20Nov/'
 config.General.transferLogs = True
 
 config.section_("JobType")
@@ -15,14 +15,14 @@ config.section_("Data")
 config.Data.inputDataset = '/DoubleMuon/devdatta-Run2015D-PromptReco-v4_B2GAnaFW_v74x_v8p4-5daaa7fbf157b0642c1d3dfb260fbeff/USER'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'LumiBased'
-config.Data.unitsPerJob = 10
+config.Data.unitsPerJob = 20
 config.Data.ignoreLocality = False
 config.Data.publication = True
-config.Data.outLFNDirBase = '/store/group/phys_b2g/B2GAnaFW/Skims/CR_Zmumu/'
-config.Data.outputDatasetTag = 'Run2015D-PromptReco-v4_B2GAnaFW_v74x_v8p4_Skim_CR_Zmumu_24Nov2015'
+config.Data.outLFNDirBase = '/store/group/lpcbprime/noreplica/skhalil/B2GEDMNtuplesSkim_CR_Zmumu_20Nov/'
+config.Data.outputDatasetTag = 'Run2015D-PromptReco-v4_B2GAnaFW_v74x_v8p4_Skim_CR_Zmumu_04Feb2016'
 # This string is used to construct the output dataset name
 
 config.section_("Site")
-config.Site.storageSite = 'T2_CH_CERN'
+config.Site.storageSite = 'T3_US_FNALLPC'
 
 config.section_('User')
