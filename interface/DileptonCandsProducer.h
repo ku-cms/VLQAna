d6eff82ec8e44b9d08d218d12df7dae2b69c6525
skhalil <skhalil@fnal.gov>
2016-04-04 16:57:32 -0500

    changed the order of cuts if cutflow, require leading lepton pt > 45 GeV, b-jet pt > 50 GeV, and use only matched ak8 jets  (to ak4) in event

--- interface/DileptonCandsProducer.h
+++ interface/DileptonCandsProducer.h
@@ -13,25 +13,28 @@ class DileptonCandsProducer {
       massMin_(iConfig.getParameter<double> ("massMin")), 
       massMax_(iConfig.getParameter<double> ("massMax")), 
       ptMin_(iConfig.getParameter<double> ("ptMin")), 
-      ptMax_(iConfig.getParameter<double> ("ptMax")) 
+      ptMax_(iConfig.getParameter<double> ("ptMax")),
+      ptMaxLeadingLep_(iConfig.getParameter<double> ("ptMaxLeadingLep")),
+      ptMax2ndLeadingLep_(iConfig.getParameter<double> ("ptMax2ndLeadingLep"))  
   {}
 
     template <class T>
     void operator () (vlq::CandidateCollection& zcands, const T& leptons) {
-
-      for ( auto l1 = leptons.begin(); l1 != leptons.end(); ++l1) {
-        for ( auto l2 = std::next(l1); l2 != leptons.end(); ++l2) {
-          if (l1->getCharge()*l2->getCharge() != -1 ) continue ; 
-          TLorentzVector p4l1(l1->getP4()), p4l2(l2->getP4()) ;
-          double mass = (p4l1+p4l2).Mag() ; 
-          double pt = (p4l1+p4l2).Pt() ; 
-          if ( mass > massMin_ && mass < massMax_ && pt > ptMin_ && pt < ptMax_ ) {
-            vlq::Candidate zll(p4l1+p4l2) ; 
+      for ( auto l1 = leptons.begin(); l1 != leptons.end(); ++l1) {    
+        for ( auto l2 = std::next(l1); l2 != leptons.end(); ++l2) {          
+           if (l1->getCharge()*l2->getCharge() != -1 ) continue ;
+           //cout << "1st, 2nd lep pt = " << l1->getPt() << " ," << l2->getPt() << endl;   
+           if (l1->getPt() < ptMaxLeadingLep_) continue;
+           if (l2->getPt() < ptMax2ndLeadingLep_) continue;  
+           TLorentzVector p4l1(l1->getP4()), p4l2(l2->getP4()) ;
+           double mass = (p4l1+p4l2).Mag() ; 
+           double pt = (p4l1+p4l2).Pt() ; 
+           if ( mass > massMin_ && mass < massMax_ && pt > ptMin_ && pt < ptMax_ ) {                   
+              vlq::Candidate zll(p4l1+p4l2) ; 
             zcands.push_back(zll) ; 
-          }
+           }
         }
       }
-
       return ;
     }
 
@@ -42,5 +45,7 @@ class DileptonCandsProducer {
     double massMax_ ; 
     double ptMin_ ; 
     double ptMax_ ; 
+    double ptMaxLeadingLep_;
+    double ptMax2ndLeadingLep_;
 }; 
 #endif 
--- python/ZCandSelector_cfi.py
+++ python/ZCandSelector_cfi.py
@@ -4,7 +4,9 @@ defaultZCandSelectionParameters = cms.PSet(
     massMin = cms.double(50),
     massMax = cms.double(100000),
     ptMin = cms.double(0),
-    ptMax = cms.double(100000), 
+    ptMax = cms.double(100000),
+    ptMaxLeadingLep = cms.double(25),
+    ptMax2ndLeadingLep = cms.double(25)
     )
 
 
