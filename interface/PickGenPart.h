a43971f9e70cde8f51ae24d260bc7e55a1f8ee5b
Devdatta <devdatta.majumder@cern.ch>
2016-05-03 00:09:47 +0200

    Moving to using getByToken

--- interface/PickGenPart.h
+++ interface/PickGenPart.h
@@ -17,22 +17,22 @@ class PickGenPart {
 
   private: 
 
-    edm::InputTag l_genPartID          ; 
-    edm::InputTag l_genPartStatus      ; 
-    edm::InputTag l_genPartMom0ID      ; 
-    edm::InputTag l_genPartMom1ID      ; 
-    edm::InputTag l_genPartMom0Status  ; 
-    edm::InputTag l_genPartMom1Status  ; 
-    edm::InputTag l_genPartDau0ID      ; 
-    edm::InputTag l_genPartDau1ID      ; 
-    edm::InputTag l_genPartDau0Status  ; 
-    edm::InputTag l_genPartDau1Status  ; 
-    edm::InputTag l_genPartPt          ; 
-    edm::InputTag l_genPartEta         ; 
-    edm::InputTag l_genPartPhi         ; 
-    edm::InputTag l_genPartMass        ; 
-    edm::InputTag l_genPartE           ; 
-    edm::InputTag l_genPartCharge      ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartID          ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartStatus      ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartMom0ID      ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartMom1ID      ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartMom0Status  ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartMom1Status  ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartDau0ID      ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartDau1ID      ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartDau0Status  ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartDau1Status  ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartPt          ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartEta         ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartPhi         ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartMass        ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartE           ; 
+    edm::EDGetTokenT<std::vector<float>> t_genPartCharge      ; 
 
     std::vector<int> ids_ ; 
     std::vector<int> statuses_ ; 
--- src/PickGenPart.cc
+++ src/PickGenPart.cc
@@ -5,22 +5,22 @@
 using namespace edm;
 
 PickGenPart::PickGenPart (const edm::ParameterSet& iConfig, edm::ConsumesCollector && iC) :
-  l_genPartID          (iConfig.getParameter<edm::InputTag>("genPartID")), 
-  l_genPartStatus      (iConfig.getParameter<edm::InputTag>("genPartStatus")),
-  l_genPartMom0ID      (iConfig.getParameter<edm::InputTag>("genPartMom0ID")), 
-  l_genPartMom1ID      (iConfig.getParameter<edm::InputTag>("genPartMom1ID")), 
-  l_genPartMom0Status  (iConfig.getParameter<edm::InputTag>("genPartMom0Status")), 
-  l_genPartMom1Status  (iConfig.getParameter<edm::InputTag>("genPartMom1Status")), 
-  l_genPartDau0ID      (iConfig.getParameter<edm::InputTag>("genPartDau0ID")), 
-  l_genPartDau1ID      (iConfig.getParameter<edm::InputTag>("genPartDau1ID")), 
-  l_genPartDau0Status  (iConfig.getParameter<edm::InputTag>("genPartDau0Status")), 
-  l_genPartDau1Status  (iConfig.getParameter<edm::InputTag>("genPartDau1Status")), 
-  l_genPartPt          (iConfig.getParameter<edm::InputTag>("genPartPt")), 
-  l_genPartEta         (iConfig.getParameter<edm::InputTag>("genPartEta")), 
-  l_genPartPhi         (iConfig.getParameter<edm::InputTag>("genPartPhi")), 
-  l_genPartMass        (iConfig.getParameter<edm::InputTag>("genPartMass")), 
-  l_genPartE           (iConfig.getParameter<edm::InputTag>("genPartE")), 
-  l_genPartCharge      (iConfig.getParameter<edm::InputTag>("genPartCharge")), 
+  t_genPartID          (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartID"))), 
+  t_genPartStatus      (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartStatus"))),
+  t_genPartMom0ID      (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartMom0ID"))), 
+  t_genPartMom1ID      (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartMom1ID"))), 
+  t_genPartMom0Status  (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartMom0Status"))), 
+  t_genPartMom1Status  (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartMom1Status"))), 
+  t_genPartDau0ID      (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartDau0ID"))), 
+  t_genPartDau1ID      (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartDau1ID"))), 
+  t_genPartDau0Status  (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartDau0Status"))), 
+  t_genPartDau1Status  (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartDau1Status"))), 
+  t_genPartPt          (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartPt"))), 
+  t_genPartEta         (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartEta"))), 
+  t_genPartPhi         (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartPhi"))), 
+  t_genPartMass        (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartMass"))), 
+  t_genPartE           (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartE"))), 
+  t_genPartCharge      (iC.consumes<std::vector<float>>(iConfig.getParameter<edm::InputTag>("genPartCharge"))), 
   ids_                 (iConfig.getParameter<std::vector<int>>("ids")), 
   statuses_            (iConfig.getParameter<std::vector<int>>("statuses")), 
   checkstatus_         (iConfig.getParameter<bool>("checkstatus")), 
@@ -32,24 +32,6 @@ PickGenPart::PickGenPart (const edm::ParameterSet& iConfig, edm::ConsumesCollect
   checkdauid_          (iConfig.getParameter<bool>("checkdauid")),
   debug_               (iConfig.getParameter<bool>("debug"))
 { 
-
-  iC.consumes<std::vector<float>>(l_genPartID        ); 
-  iC.consumes<std::vector<float>>(l_genPartStatus    ); 
-  iC.consumes<std::vector<float>>(l_genPartMom0ID    ); 
-  iC.consumes<std::vector<float>>(l_genPartMom1ID    ); 
-  iC.consumes<std::vector<float>>(l_genPartMom0Status); 
-  iC.consumes<std::vector<float>>(l_genPartMom1Status); 
-  iC.consumes<std::vector<float>>(l_genPartDau0ID    ); 
-  iC.consumes<std::vector<float>>(l_genPartDau1ID    ); 
-  iC.consumes<std::vector<float>>(l_genPartDau0Status); 
-  iC.consumes<std::vector<float>>(l_genPartDau1Status); 
-  iC.consumes<std::vector<float>>(l_genPartPt        ); 
-  iC.consumes<std::vector<float>>(l_genPartEta       ); 
-  iC.consumes<std::vector<float>>(l_genPartPhi       ); 
-  iC.consumes<std::vector<float>>(l_genPartMass      ); 
-  iC.consumes<std::vector<float>>(l_genPartE         ); 
-  iC.consumes<std::vector<float>>(l_genPartCharge    ); 
-
 }
 
 PickGenPart::~PickGenPart () {}
@@ -60,22 +42,22 @@ const GenParticleCollection PickGenPart::operator() ( edm::Event& evt) {
 
   typedef edm::Handle <std::vector<float>> hfloat ; 
 
-  hfloat h_genPartPt            ; evt.getByLabel(l_genPartPt            , h_genPartPt           ) ;  
-  hfloat h_genPartEta           ; evt.getByLabel(l_genPartEta           , h_genPartEta          ) ;  
-  hfloat h_genPartPhi           ; evt.getByLabel(l_genPartPhi           , h_genPartPhi          ) ;  
-  hfloat h_genPartMass          ; evt.getByLabel(l_genPartMass          , h_genPartMass         ) ;  
-  hfloat h_genPartE             ; evt.getByLabel(l_genPartE             , h_genPartE            ) ;  
-  hfloat h_genPartCharge        ; evt.getByLabel(l_genPartCharge        , h_genPartCharge       ) ;  
-  hfloat h_genPartID            ; evt.getByLabel(l_genPartID            , h_genPartID           ) ;  
-  hfloat h_genPartStatus        ; evt.getByLabel(l_genPartStatus        , h_genPartStatus       ) ;  
-  hfloat h_genPartMom0ID        ; evt.getByLabel(l_genPartMom0ID        , h_genPartMom0ID       ) ;  
-  hfloat h_genPartMom1ID        ; evt.getByLabel(l_genPartMom1ID        , h_genPartMom1ID       ) ;  
-  hfloat h_genPartMom0Status    ; evt.getByLabel(l_genPartMom0Status    , h_genPartMom0Status   ) ;  
-  hfloat h_genPartMom1Status    ; evt.getByLabel(l_genPartMom1Status    , h_genPartMom1Status   ) ;  
-  hfloat h_genPartDau0ID        ; evt.getByLabel(l_genPartDau0ID        , h_genPartDau0ID       ) ;  
-  hfloat h_genPartDau1ID        ; evt.getByLabel(l_genPartDau1ID        , h_genPartDau1ID       ) ;  
-  hfloat h_genPartDau0Status    ; evt.getByLabel(l_genPartDau0Status    , h_genPartDau0Status   ) ;  
-  hfloat h_genPartDau1Status    ; evt.getByLabel(l_genPartDau1Status    , h_genPartDau1Status   ) ;  
+  hfloat h_genPartPt            ; evt.getByToken(t_genPartPt            , h_genPartPt           ) ;  
+  hfloat h_genPartEta           ; evt.getByToken(t_genPartEta           , h_genPartEta          ) ;  
+  hfloat h_genPartPhi           ; evt.getByToken(t_genPartPhi           , h_genPartPhi          ) ;  
+  hfloat h_genPartMass          ; evt.getByToken(t_genPartMass          , h_genPartMass         ) ;  
+  hfloat h_genPartE             ; evt.getByToken(t_genPartE             , h_genPartE            ) ;  
+  hfloat h_genPartCharge        ; evt.getByToken(t_genPartCharge        , h_genPartCharge       ) ;  
+  hfloat h_genPartID            ; evt.getByToken(t_genPartID            , h_genPartID           ) ;  
+  hfloat h_genPartStatus        ; evt.getByToken(t_genPartStatus        , h_genPartStatus       ) ;  
+  hfloat h_genPartMom0ID        ; evt.getByToken(t_genPartMom0ID        , h_genPartMom0ID       ) ;  
+  hfloat h_genPartMom1ID        ; evt.getByToken(t_genPartMom1ID        , h_genPartMom1ID       ) ;  
+  hfloat h_genPartMom0Status    ; evt.getByToken(t_genPartMom0Status    , h_genPartMom0Status   ) ;  
+  hfloat h_genPartMom1Status    ; evt.getByToken(t_genPartMom1Status    , h_genPartMom1Status   ) ;  
+  hfloat h_genPartDau0ID        ; evt.getByToken(t_genPartDau0ID        , h_genPartDau0ID       ) ;  
+  hfloat h_genPartDau1ID        ; evt.getByToken(t_genPartDau1ID        , h_genPartDau1ID       ) ;  
+  hfloat h_genPartDau0Status    ; evt.getByToken(t_genPartDau0Status    , h_genPartDau0Status   ) ;  
+  hfloat h_genPartDau1Status    ; evt.getByToken(t_genPartDau1Status    , h_genPartDau1Status   ) ;  
 
   for ( unsigned igen = 0; igen < (h_genPartID.product())->size(); ++igen ) {
 
