ce2234e776d9db91be568a6c31062c06cda611f4
Devdatta <devdatta.majumder@cern.ch>
2016-05-03 00:10:17 +0200

    Modified sel

--- test/os2lana_cfg.py
+++ test/os2lana_cfg.py
@@ -48,31 +48,36 @@ options.register('FileNames', 'FileNames_QCD_HT1000to1500',
     VarParsing.varType.string,
     "Name of list of input files"
     )
+options.register('optimizeReco', True,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.bool,
+    "Optimize mass reconstruction"
+    )
 
 options.setDefault('maxEvents', -1)
 options.parseArguments()
 print options
 
 hltpaths = []
-if options.zdecaymode == "zmumu":
-  hltpaths = [
-      "HLT_DoubleIsoMu17_eta2p1_v", 
-      "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v",
-      "HLT_DoubleMu8_Mass8_PFHT300_v",
-      ]
-elif options.zdecaymode == "zelel":
-  hltpaths = [
-      "HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_v",
-      "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
-      "HLT_DoubleEle8_CaloIdM_TrackIdM_Mass8_PFHT300_v"
-      ]
-else:
-  sys.exit("!!!Error: Wrong Z decay mode option chosen. Choose either 'zmumu' or 'zelel'!!!") 
-
 if options.isData:
   options.filterSignal = False 
   options.signalType = "" 
+  options.optimizeReco = False
   options.applyLeptonSFs = False 
+  if options.zdecaymode == "zmumu":
+    hltpaths = [
+        "HLT_DoubleIsoMu17_eta2p1_v", 
+        "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v",
+        #"HLT_DoubleMu8_Mass8_PFHT300_v",
+        ]
+  elif options.zdecaymode == "zelel":
+    hltpaths = [
+        "HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_v",
+        "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
+        #"HLT_DoubleEle8_CaloIdM_TrackIdM_Mass8_PFHT300_v"
+        ]
+  else:
+    sys.exit("!!!Error: Wrong Z decay mode option chosen. Choose either 'zmumu' or 'zelel'!!!") 
 
 if options.filterSignal == True and len(options.signalType) == 0:
   sys.exit("!!!Error: Cannot keep signalType empty when filterSignal switched on!!!")  
@@ -84,8 +89,9 @@ from inputFiles_cfi import *
 process.source = cms.Source(
     "PoolSource",
     fileNames = cms.untracked.vstring(
-      FileNames[options.FileNames]
+      #FileNames[options.FileNames]
       #'file:/afs/cern.ch/work/d/devdatta/CMSREL/B2GAnaFW_76X/CMSSW_7_6_3_patch2/src/Analysis/B2GAnaFW/test/B2GEDMNtuple_DYJets_M50Madgraph.root',
+      'root://cms-xrd-global.cern.ch//store/user/jkarancs/SusyAnalysis/B2GEdmNtuple/TT_TuneCUETP8M1noCR_13TeV-powheg-pythia8/B2GAnaFW_76X_V1p1_RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/160401_092930/0000/B2GEDMNtuple_13.root'
     ) 
     )
 
@@ -107,6 +113,7 @@ process.ana = ana.clone(
     signalType = cms.string(options.signalType),
     zdecayMode = cms.string(options.zdecaymode),
     applyLeptonSFs = cms.bool(options.applyLeptonSFs),
+    optimizeReco = cms.bool(options.optimizeReco),
     )
 process.ana.elselParams.elidtype = cms.string(options.lepID)
 process.ana.muselParams.muidtype = cms.string(options.lepID)
@@ -116,7 +123,9 @@ process.ana.lepsfsParams.zdecayMode = cms.string(options.zdecaymode)
 process.ana.BoostedZCandParams.ptMin = cms.double(80.)
 process.ana.jetAK8selParams.jetPtMin = cms.double(200) 
 process.ana.jetAK4BTaggedselParams.jetPtMin = cms.double(50) 
-process.ana.STMin = cms.double(500.)
+process.ana.STMin = cms.double(1000.)
+process.ana.vlqMass = cms.double(1000.) #M=1000
+process.ana.bosonMass = cms.double(91.2) #Z
 
 process.TFileService = cms.Service("TFileService",
        fileName = cms.string(
