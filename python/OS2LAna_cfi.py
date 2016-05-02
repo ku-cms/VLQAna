6c5e683b3fe187389f3a11263071bc0959bb29d1
Devdatta <devdatta.majumder@cern.ch>
2016-05-03 00:10:04 +0200

    Modified cuts

--- python/OS2LAna_cfi.py
+++ python/OS2LAna_cfi.py
@@ -6,6 +6,7 @@ from Analysis.VLQAna.ElectronMaker_cfi import *
 from Analysis.VLQAna.MuonMaker_cfi import *
 from Analysis.VLQAna.PickGenPart_cfi import *
 from Analysis.VLQAna.JetSelector_cfi import *
+from Analysis.VLQAna.METMaker_cfi import *
 
 ana = cms.EDFilter("OS2LAna",
     hltdecision                = cms.InputTag("evtcleaner","hltdecision"),   
@@ -17,6 +18,9 @@ ana = cms.EDFilter("OS2LAna",
     additionalPlots            = cms.bool(False), 
     signalType                 = cms.string(""), 
     zdecayMode                 = cms.string("zelel"),
+    optimizeReco               = cms.bool(False),
+    vlqMass                    = cms.double(1000.),
+    bosMass                    = cms.double(91.2),
     lep                        = cms.string("el"),
     applyLeptonSFs             = cms.bool(True),  
     DoPUReweightingNPV         = cms.bool(False),
@@ -36,11 +40,13 @@ ana = cms.EDFilter("OS2LAna",
         ptMin = cms.double(200.),
         ), 
     GenHSelParams              = genPartParams.clone(), 
-    STMin                      = cms.double  (500.), 
+    STMin                      = cms.double  (500.),
+    HTMin                      = cms.double (150.),
     lepsfsParams               = defaultWP.clone(
         lepidtype = cms.string("TIGHT"),
         zdecayMode = cms.string("zelel"),
         ),
+    metselParams               = defaultMETMakerParameters,
     muselParams                = defaultMuonMakerParameters, 
     elselParams                = defaultElectronMakerParameters, 
     jetAK4selParams            = defaultAK4JetSelectionParameters,
@@ -48,5 +54,6 @@ ana = cms.EDFilter("OS2LAna",
     jetAK8selParams            = defaultAK8JetSelectionParameters,
     jetHTaggedselParams        = defaultHJetSelectionParameters,
     jetWTaggedselParams        = defaultWJetSelectionParameters,
-    jetTopTaggedselParams      = defaultTJetSelectionParameters, 
+    jetTopTaggedselParams      = defaultTJetSelectionParameters,
+    genParams                  = getSelectParams,
     )
