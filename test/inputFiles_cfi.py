ef891b4e66a8255ba7310d3c0691f22693f1cb25
Devdatta <devdatta.majumder@cern.ch>
2016-08-22 20:01:35 +0200

    New B2G NTuples

--- test/inputFiles_cfi.py
+++ test/inputFiles_cfi.py
@@ -10,5 +10,8 @@ FileNames = {
     'file:/afs/cern.ch/user/d/devdatta/eos/cms/store/group/phys_b2g/B2GAnaFW_80X_V1p2/Spring16_25nsV3/TprimeBToTZ_M-1000_LH_TuneCUETP8M1_13TeV-madgraph-pythia8/B2GAnaFW_80X_V1p2_Spring16_25nsV3/160706_182209/0000/B2GEDMNtuple_7.root', 
     'file:/afs/cern.ch/user/d/devdatta/eos/cms/store/group/phys_b2g/B2GAnaFW_80X_V1p2/Spring16_25nsV3/TprimeBToTZ_M-1000_LH_TuneCUETP8M1_13TeV-madgraph-pythia8/B2GAnaFW_80X_V1p2_Spring16_25nsV3/160706_182209/0000/B2GEDMNtuple_6.root', 
   ],
+  'FileNames_BG1000_AK8Reg': [
+    'file:/afs/cern.ch/user/d/devdatta/eos/cms/store/user/devdatta/B2GAnaFW_v80x_v2p0_NewGenPartFormat/BulkGravTohhTohbbhbb_narrow_M-2500_13TeV-madgraph/v80x_v2p0_NewGenPartFormat/160822_124214/0000/B2GEDMNtuple_2.root',
+    ]
 }
 files=[]
