034cf966176d5003e99da342738a8588a7e9c99c
Erich <erichjs@hotmail.com>
2016-11-02 11:12:40 -0500

    for running over multiple hltpaths

--- test/vlqana_cfg.py
+++ test/vlqana_cfg.py
@@ -63,7 +63,7 @@ options.register('storeLHEWts', True,
     VarParsing.varType.bool,
     "Store LHE wts?"
     )
-options.register('FileNames', 'FileNames_QCD_HT1000to1500',
+options.register('FileNames', 'FileNames_BG2000',
     VarParsing.multiplicity.singleton,
     VarParsing.varType.string,
     "Name of list of input files"
@@ -72,7 +72,10 @@ options.register('FileNames', 'FileNames_QCD_HT1000to1500',
 options.setDefault('maxEvents', -1000)
 options.parseArguments()
 
-hltpaths = ["HLT_PFHT800_v"]
+hltpaths = ["HLT_AK8DiPFJet280_200_TrimMass30_BTagCSV_p20_v"]
+hltpaths_PFHT800 = ["HLT_PFHT800_v"]
+hltpaths_test = ["HLT_PFHT800_v","HLT_AK8DiPFJet280_200_TrimMass30_BTagCSV_p20_v"]
+hltpaths_loose = ["HLT_AK8DiPFJet280_200_TrimMass30_BTagCSV_p20_v"]
 if options.isData:
     options.doBTagSFUnc = False 
     options.jerShift = 0 
@@ -111,10 +114,15 @@ process.TFileService = cms.Service("TFileService",
 #    )
 
 process.load("Analysis.VLQAna.EventCleaner_cff") 
-process.evtcleaner.hltPaths = cms.vstring (hltpaths)  
+process.evtcleaner.hltPaths = cms.vstring (hltpaths_loose)  
 process.evtcleaner.isData = options.isData 
 process.evtcleaner.DoPUReweightingOfficial = options.doPUReweightingOfficial
 process.evtcleaner.storeLHEWts = options.storeLHEWts
+
+process.evtcleanerPFHT800.hltPaths = cms.vstring (hltpaths_test)  
+process.evtcleanerPFHT800.isData = options.isData 
+process.evtcleanerPFHT800.DoPUReweightingOfficial = options.doPUReweightingOfficial
+process.evtcleanerPFHT800.storeLHEWts = options.storeLHEWts
 
 process.load("Analysis.VLQAna.VLQAna_cfi") 
 process.ana.doBTagSFUnc = options.doBTagSFUnc
@@ -134,10 +142,31 @@ process.ana.storePreselEvts = options.storePreselEvts
 process.ana.doPreselOnly = options.doPreselOnly
 process.ana.HTMin = HTMin
 
+process.anaPFHT800.doBTagSFUnc = options.doBTagSFUnc
+process.anaPFHT800.jetAK4selParams.jecShift = options.jecShift 
+process.anaPFHT800.jetAK4selParams.jerShift = options.jerShift 
+process.anaPFHT800.jetAK8selParams.jecShift = options.jecShift 
+process.anaPFHT800.jetAK8selParams.jerShift = options.jerShift 
+process.anaPFHT800.jetHTaggedselParams.jecShift = options.jecShift 
+process.anaPFHT800.jetHTaggedselParams.jerShift = options.jerShift 
+process.anaPFHT800.jetTopTaggedselParams.jecShift = options.jecShift 
+process.anaPFHT800.jetTopTaggedselParams.jerShift = options.jerShift 
+process.anaPFHT800.jetAntiHTaggedselParams.jecShift = options.jecShift 
+process.anaPFHT800.jetAntiHTaggedselParams.jerShift = options.jerShift 
+process.anaPFHT800.jetTopTaggedselParams.jettau3Bytau2Max = options.topTagtau32
+process.anaPFHT800.jetTopTaggedselParams.subjetHighestCSVMin = options.topTagBDisc
+process.anaPFHT800.storePreselEvts = options.storePreselEvts
+process.anaPFHT800.doPreselOnly = options.doPreselOnly
+process.anaPFHT800.HTMin = HTMin
+
 from Analysis.EventCounter.eventcounter_cfi import eventCounter
 process.allEvents = eventCounter.clone(isData=options.isData)
 process.cleanedEvents = eventCounter.clone(isData=options.isData)
 process.finalEvents = eventCounter.clone(isData=options.isData)
+process.allEventsTestHLT = eventCounter.clone(isData=options.isData)
+process.cleanedEventsTestHLT = eventCounter.clone(isData=options.isData)
+process.finalEventsTestHLT = eventCounter.clone(isData=options.isData)
+
 
 process.p = cms.Path(
     process.allEvents
@@ -146,7 +175,11 @@ process.p = cms.Path(
     *process.ana 
     *process.finalEvents
     )
-
+process.p2 = cms.Path(
+    process.evtcleanerPFHT800
+    *process.anaPFHT800
+    *process.finalEventsTestHLT
+    ) 
 #process.outpath = cms.EndPath(process.out)
 
 open('dump.py','w').write(process.dumpPython())
