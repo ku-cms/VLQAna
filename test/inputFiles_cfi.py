693e3251d354f023ea8a6eab65ace3f4f59c57b7
Devdatta <devdatta.majumder@cern.ch>
2016-11-11 18:43:54 +0100

    updating with TT root file

--- test/inputFiles_cfi.py
+++ test/inputFiles_cfi.py
@@ -1,6 +1,9 @@
 FileNames = {
     'FileNames': [
       ],
+    'FileNames_TT':[
+      '/store/user/grauco/B2GAnaFW/B2GAnaFW_80X_V2p1/TT_TuneCUETP8M1_13TeV-powheg-pythia8/B2GAnaFW_80X_V2p1/161021_085128/0000/B2GEDMNtuple_105.root'
+      ],
     'FileNames_BG2000': [
       'file:/afs/cern.ch/user/d/devdatta/eos/cms/store/group/phys_b2g/B2GAnaFW_80X_V2p1/BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph/RunIISpring16MiniAODv2/BulkGravTohhTohbbhbb_narrow_M-2000_13TeV-madgraph/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1_B2GAnaFW_80X_V2p1/161013_161735/0000/B2GEDMNtuple_1.root'
       ],
