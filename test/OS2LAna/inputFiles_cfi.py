bda7e65e7fd8fffeb59ed82acc90bd043f26f384
Devdatta <devdatta.majumder@cern.ch>
2016-12-20 14:18:53 +0100

    updating inputs

---
+++ test/OS2LAna/inputFiles_cfi.py
@@ -0,0 +1,17 @@
+FileNames = {
+    'FileNames': [
+      ],
+    'FileNames_DY_pt_650ToInf':[
+      'root://eoscms.cern.ch//store/group/phys_b2g/B2GAnaFW_80X_V2p1/DYJetsToLL_Pt-650ToInf_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/B2GAnaFW_RunIISpring16MiniAODv2_25ns_v80x_v2p1/161119_100629/0000/B2GEDMNtuple_2.root',
+      'root://eoscms.cern.ch//store/group/phys_b2g/B2GAnaFW_80X_V2p1/DYJetsToLL_Pt-650ToInf_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/B2GAnaFW_RunIISpring16MiniAODv2_25ns_v80x_v2p1/161119_100629/0000/B2GEDMNtuple_3.root',
+      ],
+    'FileNames_BpBp1800':[
+      '/store/group/phys_b2g/B2GAnaFW_80X_V2p1/BprimeBprime_M-1800_TuneCUETP8M1_13TeV-madgraph-pythia8/B2GAnaFW_RunIISpring16MiniAODv2_25ns_v80x_v2p1/161122_174707/0000/B2GEDMNtuple_1.root', 
+      ],
+    'FileNames_TpTp1200':[
+      '/store/group/phys_b2g/B2GAnaFW_80X_V2p1/TprimeTprime_M-1200_TuneCUETP8M1_13TeV-madgraph-pythia8/B2GAnaFW_RunIISpring16MiniAODv2_25ns_v80x_v2p1/161122_092031/0000/B2GEDMNtuple_4.root',
+      ],
+    'FileNames_TT':[
+      '/store/user/grauco/B2GAnaFW/B2GAnaFW_80X_V2p1/TT_TuneCUETP8M1_13TeV-powheg-pythia8/B2GAnaFW_80X_V2p1/161021_085128/0000/B2GEDMNtuple_105.root'
+      ],
+  }
