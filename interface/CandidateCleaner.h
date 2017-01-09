93ccab431c8fae73d1dd6c8f7974bfed1b092f63
Devdatta <devdatta.majumder@cern.ch>
2017-01-09 15:07:19 +0100

    lepton 2D isolation

--- interface/CandidateCleaner.h
+++ interface/CandidateCleaner.h
@@ -3,16 +3,32 @@
 #include <vector>
 #include "AnalysisDataFormats/BoostedObjects/interface/Candidate.h"
 
+///
+/// Class to clean one set of candidates T1 from another T2:
+/// Configurables:
+///   dr_ = Separation in DR(eta, phi) between the two cadidates.
+///   ptrel_ = relative pt between the two candidates (used for lepton 2D isolation). 
+///   If ptrel_ < 0 (negative), 2D isolation is not applied, and an ordinary DR cleaning is used.
+///   If ptrel_ > 0, T1 collection are cleaned from T2 if dr(t1, t2) > dr_ *or* if pt(t1)/pt(t2) > ptrel_
+///
+
 class CandidateCleaner {
   public:
-    CandidateCleaner(double dr) : dr_(dr) {}
+    CandidateCleaner(double dr, double ptrel) : dr_(dr), ptrel_(ptrel) {}
     ~CandidateCleaner() {}
     template <class T1, class T2>
       void operator () (T1& cleanedcands, T2 othercands) {
         for (typename  T1::iterator icand = cleanedcands.begin(); icand < cleanedcands.end(); ++icand) {
           bool isclean(true); 
           for (auto othercand : othercands) {
-            if ( (icand->getP4()).DeltaR(othercand.getP4()) < dr_ ) { isclean = false; break ; }  
+            double dr((icand->getP4()).DeltaR(othercand.getP4()));
+            if (ptrel_ < 0) {
+              if ( dr < dr_ ) { isclean = false; break ; }  
+            }
+            else { //// Apply 2D isolation
+              double ptrel ( ( ((icand->getP4()).Vect()).Cross((othercand.getP4()).Vect()) ).Mag()/(othercand.getP4()).Mag() ) ;
+              if ( dr < dr_ && ptrel < ptrel_ ) { isclean = false; break ; }  
+            }
           }
           if ( !isclean ) cleanedcands.erase(icand) ; 
         }
@@ -20,5 +36,6 @@ class CandidateCleaner {
 
   private:
     double dr_;
+    double ptrel_;
 }; 
 #endif
