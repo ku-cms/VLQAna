a3f66638cd1205414b15e1ade333ece432cc7bbf
Devdatta <devdatta.majumder@cern.ch>
2017-01-26 00:27:58 +0100

    adding evtwt to event and higgs jets

--- plugins/OS2LAna.cc
+++ plugins/OS2LAna.cc
@@ -211,10 +211,12 @@ OS2LAna::OS2LAna(const edm::ParameterSet& iConfig) :
 {
   produces<vlq::JetCollection>("tjets") ; 
   produces<vlq::JetCollection>("wjets") ; 
+  produces<vlq::JetCollection>("hjets") ;
   produces<vlq::JetCollection>("bjets") ; 
   produces<vlq::JetCollection>("jets") ; 
   produces<vlq::CandidateCollection>("zllcands") ; 
   produces<double>("PreWeight");
+  produces<double>("finalWeight");
 }
 
 
@@ -720,22 +722,28 @@ bool OS2LAna::filter(edm::Event& evt, const edm::EventSetup& iSetup) {
   h1_["sqrtChi2"] ->Fill(sqrt(chi2_result.first), evtwt);
   h1_["chi2_mass"] ->Fill(chi2_result.second, evtwt);
 */
+  std::unique_ptr<double> ptr_fevtwt ( new double(evtwt) ) ;
+  std::unique_ptr<double> ptr_evtwt ( new double(presel_wt) ) ;
+  evt.put(std::move(ptr_fevtwt), "finalWeight");
+  evt.put(std::move(ptr_evtwt), "PreWeight");
   if (skim_){
-     std::auto_ptr<double> ptr_evtwt ( new double(presel_wt) ) ; 
-     evt.put(ptr_evtwt, "PreWeight");
+     //std::unique_ptr<double> ptr_evtwt ( new double(presel_wt) ) ; 
+     //evt.put(std::move(ptr_evtwt), "PreWeight");
      
      if(goodAK4Jets.at(0).getPt() > 100 && goodAK4Jets.at(1).getPt() > 50 && goodBTaggedAK4Jets.size() > 0 && ST > STMin_){
-        std::auto_ptr<vlq::JetCollection> ptr_tjets( new vlq::JetCollection(goodTopTaggedJets) ) ; 
-        std::auto_ptr<vlq::JetCollection> ptr_wjets( new vlq::JetCollection(goodWTaggedJets) ) ; 
-        std::auto_ptr<vlq::JetCollection> ptr_bjets( new vlq::JetCollection(goodBTaggedAK4Jets ) ) ; 
-        std::auto_ptr<vlq::JetCollection> ptr_jets ( new vlq::JetCollection(goodAK4Jets ) ) ; 
-        std::auto_ptr<vlq::CandidateCollection> ptr_zllcands ( new vlq::CandidateCollection(zll) ) ; 
+        std::unique_ptr<vlq::JetCollection> ptr_tjets( new vlq::JetCollection(goodTopTaggedJets) ) ; 
+        std::unique_ptr<vlq::JetCollection> ptr_wjets( new vlq::JetCollection(goodWTaggedJets) ) ; 
+        std::unique_ptr<vlq::JetCollection> ptr_hjets( new vlq::JetCollection(goodHTaggedJets) ) ;
+        std::unique_ptr<vlq::JetCollection> ptr_bjets( new vlq::JetCollection(goodBTaggedAK4Jets ) ) ; 
+        std::unique_ptr<vlq::JetCollection> ptr_jets ( new vlq::JetCollection(goodAK4Jets ) ) ; 
+        std::unique_ptr<vlq::CandidateCollection> ptr_zllcands ( new vlq::CandidateCollection(zll) ) ; 
         
-        evt.put(ptr_tjets, "tjets") ; 
-        evt.put(ptr_wjets, "wjets") ; 
-        evt.put(ptr_bjets, "bjets") ; 
-        evt.put(ptr_jets , "jets")  ; 
-        evt.put(ptr_zllcands , "zllcands")  ;
+        evt.put(std::move(ptr_tjets), "tjets") ; 
+        evt.put(std::move(ptr_wjets), "wjets") ; 
+        evt.put(std::move(ptr_hjets), "hjets") ; 
+        evt.put(std::move(ptr_bjets), "bjets") ; 
+        evt.put(std::move(ptr_jets), "jets")  ; 
+        evt.put(std::move(ptr_zllcands), "zllcands")  ;
      }   
   }
   return true ; 
