b4537a57e9547389552e86cd15a3db7ec510a905
Devdatta <devdatta.majumder@cern.ch>
2017-02-21 23:31:45 +0100

    evtwt and btag wt branches

--- interface/OS2LTree.h
+++ interface/OS2LTree.h
@@ -11,6 +11,8 @@ namespace os2l {
 
     public:
 
+      int t_signalType;
+
       unsigned int t_evtInfoLumiBlock;
       unsigned int t_evtInfoRunNumber;
       ULong64_t    t_evtInfoEventNumber;
@@ -48,47 +50,35 @@ namespace os2l {
       std::vector<float>  t_jetAK4Eta;
       std::vector<float>  t_jetAK4Phi;
       std::vector<float>  t_jetAK4E;
-      std::vector<float>  t_jetAK4GenJetPt;
-      std::vector<float>  t_jetAK4GenJetEta;
-      std::vector<float>  t_jetAK4GenJetPhi;
-      std::vector<float>  t_jetAK4GenJetE;
       std::vector<float>  t_jetAK4CSV;
       std::vector<float>  t_jetAK4Mass;
       std::vector<float>  t_jetAK4HadronFlavour;
       std::vector<float>  t_jetAK4PartonFlavour;
 
+
+      std::vector<float>  t_jetAK4GenJetPt;
+      std::vector<float>  t_jetAK4GenJetEta;
+      std::vector<float>  t_jetAK4GenJetPhi;
+      std::vector<float>  t_jetAK4GenJetE;
+
       std::vector<float>  t_jetAK4BPt;
       std::vector<float>  t_jetAK4BEta;
       std::vector<float>  t_jetAK4BPhi;
       std::vector<float>  t_jetAK4BE;
-      std::vector<float>  t_jetAK4BGenJetPt;
-      std::vector<float>  t_jetAK4BGenJetEta;
-      std::vector<float>  t_jetAK4BGenJetPhi;
-      std::vector<float>  t_jetAK4BGenJetE;
       std::vector<float>  t_jetAK4BCSV;
       std::vector<float>  t_jetAK4BMass;
       std::vector<float>  t_jetAK4BHadronFlavour;
       std::vector<float>  t_jetAK4BPartonFlavour;
 
-      double t_HT;
-      double t_ST;
-
-      double t_btagsf;
-      double t_btagsf_bcUp;
-      double t_btagsf_bcDown;
-      double t_btagsf_lUp;
-      double t_btagsf_lDown;
+      std::vector<float>  t_jetAK4BGenJetPt;
+      std::vector<float>  t_jetAK4BGenJetEta;
+      std::vector<float>  t_jetAK4BGenJetPhi;
+      std::vector<float>  t_jetAK4BGenJetE;
 
       std::vector<float>  t_jetAK8Pt;
       std::vector<float>  t_jetAK8Eta;
       std::vector<float>  t_jetAK8Phi;
       std::vector<float>  t_jetAK8E;
-
-      std::vector<float>  t_jetAK8GenJetPt;
-      std::vector<float>  t_jetAK8GenJetEta;
-      std::vector<float>  t_jetAK8GenJetPhi;
-      std::vector<float>  t_jetAK8GenJetE;
-
       std::vector<float>  t_jetAK8CSV;
       std::vector<float>  t_jetAK8Mass;
       std::vector<float>  t_jetAK8HadronFlavour;
@@ -96,10 +86,95 @@ namespace os2l {
       std::vector<float>  t_jetAK8_tau1;
       std::vector<float>  t_jetAK8_tau2;
       std::vector<float>  t_jetAK8_tau3;
-
       std::vector<float>  t_jetAK8_MassPruned;
       std::vector<float>  t_jetAK8_SoftDropMass;
       std::vector<float>  t_jetAK8_NSubJets;
+
+      std::vector<float>  t_jetAK8GenJetPt;
+      std::vector<float>  t_jetAK8GenJetEta;
+      std::vector<float>  t_jetAK8GenJetPhi;
+      std::vector<float>  t_jetAK8GenJetE;
+
+      std::vector<float>  t_jetWJetPt;
+      std::vector<float>  t_jetWJetEta;
+      std::vector<float>  t_jetWJetPhi;
+      std::vector<float>  t_jetWJetE;
+      std::vector<float>  t_jetWJetCSV;
+      std::vector<float>  t_jetWJetMass;
+      std::vector<float>  t_jetWJetHadronFlavour;
+      std::vector<float>  t_jetWJetPartonFlavour;
+      std::vector<float>  t_jetWJet_tau1;
+      std::vector<float>  t_jetWJet_tau2;
+      std::vector<float>  t_jetWJet_tau3;
+      std::vector<float>  t_jetWJet_MassPruned;
+      std::vector<float>  t_jetWJet_SoftDropMass;
+      std::vector<float>  t_jetWJet_NSubJets;
+
+      std::vector<float>  t_jetWJetGenJetPt;
+      std::vector<float>  t_jetWJetGenJetEta;
+      std::vector<float>  t_jetWJetGenJetPhi;
+      std::vector<float>  t_jetWJetGenJetE;
+
+      std::vector<float>  t_jetHJetPt;
+      std::vector<float>  t_jetHJetEta;
+      std::vector<float>  t_jetHJetPhi;
+      std::vector<float>  t_jetHJetE;
+      std::vector<float>  t_jetHJetCSV;
+      std::vector<float>  t_jetHJetMass;
+      std::vector<float>  t_jetHJetHadronFlavour;
+      std::vector<float>  t_jetHJetPartonFlavour;
+      std::vector<float>  t_jetHJet_tau1;
+      std::vector<float>  t_jetHJet_tau2;
+      std::vector<float>  t_jetHJet_tau3;
+      std::vector<float>  t_jetHJet_MassPruned;
+      std::vector<float>  t_jetHJet_SoftDropMass;
+      std::vector<float>  t_jetHJet_NSubJets;
+
+      std::vector<float>  t_jetHJetGenJetPt;
+      std::vector<float>  t_jetHJetGenJetEta;
+      std::vector<float>  t_jetHJetGenJetPhi;
+      std::vector<float>  t_jetHJetGenJetE;
+
+      std::vector<float>  t_jetTopJetPt;
+      std::vector<float>  t_jetTopJetEta;
+      std::vector<float>  t_jetTopJetPhi;
+      std::vector<float>  t_jetTopJetE;
+      std::vector<float>  t_jetTopJetCSV;
+      std::vector<float>  t_jetTopJetMass;
+      std::vector<float>  t_jetTopJetHadronFlavour;
+      std::vector<float>  t_jetTopJetPartonFlavour;
+      std::vector<float>  t_jetTopJet_tau1;
+      std::vector<float>  t_jetTopJet_tau2;
+      std::vector<float>  t_jetTopJet_tau3;
+      std::vector<float>  t_jetTopJet_MassPruned;
+      std::vector<float>  t_jetTopJet_SoftDropMass;
+      std::vector<float>  t_jetTopJet_NSubJets;
+
+      std::vector<float>  t_jetTopJetGenJetPt;
+      std::vector<float>  t_jetTopJetGenJetEta;
+      std::vector<float>  t_jetTopJetGenJetPhi;
+      std::vector<float>  t_jetTopJetGenJetE;
+
+      std::vector<float>  t_metPhi;
+      std::vector<float>  t_metPt;
+      std::vector<float>  t_metEta;
+      std::vector<float>  t_metE;
+
+      double t_HT;
+      double t_ST;
+
+      double t_presel_wt;
+      double t_evtwt;
+      double t_btagsf;
+      double t_btagsf_bcUp;
+      double t_btagsf_bcDown;
+      double t_btagsf_lUp;
+      double t_btagsf_lDown;
+      double t_sjbtagsf;
+      double t_sjbtagsf_bcUp;
+      double t_sjbtagsf_bcDown;
+      double t_sjbtagsf_lUp;
+      double t_sjbtagsf_lDown;
 
       std::vector<float>  t_genPartPt;
       std::vector<float>  t_genPartEta;
@@ -115,11 +190,6 @@ namespace os2l {
       std::vector<int>    t_genPartMom2Status;
       std::vector<int>    t_genPartDau1Status;
       std::vector<int>    t_genPartDau2Status;
-
-      std::vector<float>  t_metPhi;
-      std::vector<float>  t_metPt;
-      std::vector<float>  t_metEta;
-      std::vector<float>  t_metE;
 
       void clearTreeVectors() {
         t_TriggerNameTree     .clear();
@@ -158,6 +228,11 @@ namespace os2l {
         t_jetAK4HadronFlavour .clear();
         t_jetAK4PartonFlavour .clear();
 
+        t_jetAK4GenJetPt      .clear();
+        t_jetAK4GenJetPhi     .clear();
+        t_jetAK4GenJetEta     .clear();
+        t_jetAK4GenJetE       .clear();
+        
         t_jetAK4BPt           .clear();
         t_jetAK4BEta          .clear();
         t_jetAK4BPhi          .clear();
@@ -166,6 +241,11 @@ namespace os2l {
         t_jetAK4BMass         .clear();
         t_jetAK4BHadronFlavour.clear();
         t_jetAK4BPartonFlavour.clear();
+
+        t_jetAK4BGenJetPt     .clear();
+        t_jetAK4BGenJetPhi    .clear();
+        t_jetAK4BGenJetEta    .clear();
+        t_jetAK4BGenJetE      .clear();
 
         t_jetAK8Pt            .clear();
         t_jetAK8Eta           .clear();
@@ -182,21 +262,76 @@ namespace os2l {
         t_jetAK8_SoftDropMass .clear();
         t_jetAK8_NSubJets     .clear();
 
-        t_jetAK4GenJetPt      .clear();
-        t_jetAK4GenJetPhi     .clear();
-        t_jetAK4GenJetEta     .clear();
-        t_jetAK4GenJetE       .clear();
-        
-        t_jetAK4BGenJetPt     .clear();
-        t_jetAK4BGenJetPhi    .clear();
-        t_jetAK4BGenJetEta    .clear();
-        t_jetAK4BGenJetE      .clear();
-
         t_jetAK8GenJetPt      .clear();
         t_jetAK8GenJetEta     .clear();
         t_jetAK8GenJetPhi     .clear();
         t_jetAK8GenJetE       .clear();
 
+        t_jetWJetPt            .clear();
+        t_jetWJetEta           .clear();
+        t_jetWJetPhi           .clear();
+        t_jetWJetE             .clear();
+        t_jetWJetCSV           .clear();
+        t_jetWJetMass          .clear();
+        t_jetWJetHadronFlavour .clear();
+        t_jetWJetPartonFlavour .clear();
+        t_jetWJet_tau1         .clear();
+        t_jetWJet_tau2         .clear();
+        t_jetWJet_tau3         .clear();
+        t_jetWJet_MassPruned   .clear();
+        t_jetWJet_SoftDropMass .clear();
+        t_jetWJet_NSubJets     .clear();
+
+        t_jetWJetGenJetPt      .clear();
+        t_jetWJetGenJetEta     .clear();
+        t_jetWJetGenJetPhi     .clear();
+        t_jetWJetGenJetE       .clear();
+
+        t_jetHJetPt            .clear();
+        t_jetHJetEta           .clear();
+        t_jetHJetPhi           .clear();
+        t_jetHJetE             .clear();
+        t_jetHJetCSV           .clear();
+        t_jetHJetMass          .clear();
+        t_jetHJetHadronFlavour .clear();
+        t_jetHJetPartonFlavour .clear();
+        t_jetHJet_tau1         .clear();
+        t_jetHJet_tau2         .clear();
+        t_jetHJet_tau3         .clear();
+        t_jetHJet_MassPruned   .clear();
+        t_jetHJet_SoftDropMass .clear();
+        t_jetHJet_NSubJets     .clear();
+
+        t_jetHJetGenJetPt      .clear();
+        t_jetHJetGenJetEta     .clear();
+        t_jetHJetGenJetPhi     .clear();
+        t_jetHJetGenJetE       .clear();
+
+        t_jetTopJetPt            .clear();
+        t_jetTopJetEta           .clear();
+        t_jetTopJetPhi           .clear();
+        t_jetTopJetE             .clear();
+        t_jetTopJetCSV           .clear();
+        t_jetTopJetMass          .clear();
+        t_jetTopJetHadronFlavour .clear();
+        t_jetTopJetPartonFlavour .clear();
+        t_jetTopJet_tau1         .clear();
+        t_jetTopJet_tau2         .clear();
+        t_jetTopJet_tau3         .clear();
+        t_jetTopJet_MassPruned   .clear();
+        t_jetTopJet_SoftDropMass .clear();
+        t_jetTopJet_NSubJets     .clear();
+
+        t_jetTopJetGenJetPt      .clear();
+        t_jetTopJetGenJetEta     .clear();
+        t_jetTopJetGenJetPhi     .clear();
+        t_jetTopJetGenJetE       .clear();
+
+        t_metPt               .clear(); 
+        t_metPhi              .clear();
+        t_metEta              .clear();
+        t_metE                .clear();
+         
         t_genPartPt           .clear();
         t_genPartPhi          .clear();
         t_genPartEta          .clear();
@@ -212,15 +347,12 @@ namespace os2l {
         t_genPartDau1Status   .clear();
         t_genPartDau2Status   .clear();
 
-        t_metPt               .clear(); 
-        t_metPhi              .clear();
-        t_metEta              .clear();
-        t_metE                .clear();
-         
         return ; 
       } //// clearTreeVectors 
 
       void RegisterTree(TTree* tree, std::string name="tree") {
+
+        tree->Branch("t_signalType"          ,&t_signalType         ,"t_signalType/I" );
 
         tree->Branch("t_evtInfoLumiBlock"    ,&t_evtInfoLumiBlock   ,"t_evtInfoLumiBlock/i" );
         tree->Branch("t_evtInfoRunNumber"    ,&t_evtInfoRunNumber   ,"t_evtInfoRunNumber/i" );
@@ -265,6 +397,11 @@ namespace os2l {
         tree->Branch("t_jetAK4HadronFlavour"  ,&t_jetAK4HadronFlavour );
         tree->Branch("t_jetAK4PartonFlavour"  ,&t_jetAK4PartonFlavour );
 
+        //tree->Branch("t_jetAK4GenJetPt"  ,&t_jetAK4GenJetPt );
+        //tree->Branch("t_jetAK4GenJetPhi" ,&t_jetAK4GenJetPhi );
+        //tree->Branch("t_jetAK4GenJetEta" ,&t_jetAK4GenJetEta );
+        //tree->Branch("t_jetAK4GenJetE"   ,&t_jetAK4GenJetE );
+
         tree->Branch("t_jetAK4BPt"            ,&t_jetAK4BPt );
         tree->Branch("t_jetAK4BPhi"           ,&t_jetAK4BPhi );
         tree->Branch("t_jetAK4BEta"           ,&t_jetAK4BEta );
@@ -274,14 +411,10 @@ namespace os2l {
         tree->Branch("t_jetAK4BHadronFlavour" ,&t_jetAK4BHadronFlavour );
         tree->Branch("t_jetAK4BPartonFlavour" ,&t_jetAK4BPartonFlavour );
 
-        tree->Branch("t_HT", &t_HT, "t_HT/D") ; 
-        tree->Branch("t_ST", &t_ST, "t_ST/D") ; 
-
-        tree->Branch("t_btagsf"        ,&t_btagsf, "t_btagsf/D" );
-        tree->Branch("t_btagsf_bcUp"   ,&t_btagsf_bcUp, "t_btagsf_bcUp/D" );
-        tree->Branch("t_btagsf_bcDown" ,&t_btagsf_bcDown, "t_btagsf_bcDown/D" );
-        tree->Branch("t_btagsf_lUp"    ,&t_btagsf_lUp, "t_btagsf_lUp/D" );
-        tree->Branch("t_btagsf_lDown"  ,&t_btagsf_lDown, "t_btagsf_lDown/D" );    
+        //tree->Branch("t_jetAK4BGenJetPt"  ,&t_jetAK4BGenJetPt );
+        //tree->Branch("t_jetAK4BGenJetPhi" ,&t_jetAK4BGenJetPhi );
+        //tree->Branch("t_jetAK4BGenJetEta" ,&t_jetAK4BGenJetEta );
+        //tree->Branch("t_jetAK4BGenJetE"   ,&t_jetAK4BGenJetE );
 
         tree->Branch("t_jetAK8Pt"            ,&t_jetAK8Pt );
         tree->Branch("t_jetAK8Phi"           ,&t_jetAK8Phi );
@@ -298,20 +431,91 @@ namespace os2l {
         tree->Branch("t_jetAK8_SoftDropMass" ,&t_jetAK8_SoftDropMass);
         tree->Branch("t_jetAK8_NSubJets"     ,&t_jetAK8_NSubJets    );
 
-        tree->Branch("t_jetAK4GenJetPt"  ,&t_jetAK4GenJetPt );
-        tree->Branch("t_jetAK4GenJetPhi" ,&t_jetAK4GenJetPhi );
-        tree->Branch("t_jetAK4GenJetEta" ,&t_jetAK4GenJetEta );
-        tree->Branch("t_jetAK4GenJetE"   ,&t_jetAK4GenJetE );
+        //tree->Branch("t_jetAK8GenJetPt"  ,&t_jetAK8GenJetPt );
+        //tree->Branch("t_jetAK8GenJetPhi" ,&t_jetAK8GenJetPhi );
+        //tree->Branch("t_jetAK8GenJetEta" ,&t_jetAK8GenJetEta );
+        //tree->Branch("t_jetAK8GenJetE"   ,&t_jetAK8GenJetE );
 
-        tree->Branch("t_jetAK4BGenJetPt"  ,&t_jetAK4BGenJetPt );
-        tree->Branch("t_jetAK4BGenJetPhi" ,&t_jetAK4BGenJetPhi );
-        tree->Branch("t_jetAK4BGenJetEta" ,&t_jetAK4BGenJetEta );
-        tree->Branch("t_jetAK4BGenJetE"   ,&t_jetAK4BGenJetE );
+        tree->Branch("t_jetWJetPt"            ,&t_jetWJetPt );
+        tree->Branch("t_jetWJetPhi"           ,&t_jetWJetPhi );
+        tree->Branch("t_jetWJetEta"           ,&t_jetWJetEta );
+        tree->Branch("t_jetWJetE"             ,&t_jetWJetE );
+        tree->Branch("t_jetWJetCSV"           ,&t_jetWJetCSV );
+        tree->Branch("t_jetWJetMass"          ,&t_jetWJetMass );
+        tree->Branch("t_jetWJetHadronFlavour" ,&t_jetWJetHadronFlavour );
+        tree->Branch("t_jetWJetPartonFlavour" ,&t_jetWJetPartonFlavour );
+        tree->Branch("t_jetWJet_tau1"         ,&t_jetWJet_tau1        );
+        tree->Branch("t_jetWJet_tau2"         ,&t_jetWJet_tau2        );
+        tree->Branch("t_jetWJet_tau3"         ,&t_jetWJet_tau3        );
+        tree->Branch("t_jetWJet_MassPruned"   ,&t_jetWJet_MassPruned  );
+        tree->Branch("t_jetWJet_SoftDropMass" ,&t_jetWJet_SoftDropMass);
+        tree->Branch("t_jetWJet_NSubJets"     ,&t_jetWJet_NSubJets    );
 
-        tree->Branch("t_jetAK8GenJetPt"  ,&t_jetAK8GenJetPt );
-        tree->Branch("t_jetAK8GenJetPhi" ,&t_jetAK8GenJetPhi );
-        tree->Branch("t_jetAK8GenJetEta" ,&t_jetAK8GenJetEta );
-        tree->Branch("t_jetAK8GenJetE"   ,&t_jetAK8GenJetE );
+        //tree->Branch("t_jetWJetGenJetPt"  ,&t_jetWJetGenJetPt );
+        //tree->Branch("t_jetWJetGenJetPhi" ,&t_jetWJetGenJetPhi );
+        //tree->Branch("t_jetWJetGenJetEta" ,&t_jetWJetGenJetEta );
+        //tree->Branch("t_jetWJetGenJetE"   ,&t_jetWJetGenJetE );
+
+        tree->Branch("t_jetHJetPt"            ,&t_jetHJetPt );
+        tree->Branch("t_jetHJetPhi"           ,&t_jetHJetPhi );
+        tree->Branch("t_jetHJetEta"           ,&t_jetHJetEta );
+        tree->Branch("t_jetHJetE"             ,&t_jetHJetE );
+        tree->Branch("t_jetHJetCSV"           ,&t_jetHJetCSV );
+        tree->Branch("t_jetHJetMass"          ,&t_jetHJetMass );
+        tree->Branch("t_jetHJetHadronFlavour" ,&t_jetHJetHadronFlavour );
+        tree->Branch("t_jetHJetPartonFlavour" ,&t_jetHJetPartonFlavour );
+        tree->Branch("t_jetHJet_tau1"         ,&t_jetHJet_tau1        );
+        tree->Branch("t_jetHJet_tau2"         ,&t_jetHJet_tau2        );
+        tree->Branch("t_jetHJet_tau3"         ,&t_jetHJet_tau3        );
+        tree->Branch("t_jetHJet_MassPruned"   ,&t_jetHJet_MassPruned  );
+        tree->Branch("t_jetHJet_SoftDropMass" ,&t_jetHJet_SoftDropMass);
+        tree->Branch("t_jetHJet_NSubJets"     ,&t_jetHJet_NSubJets    );
+
+        //tree->Branch("t_jetHJetGenJetPt"  ,&t_jetHJetGenJetPt );
+        //tree->Branch("t_jetHJetGenJetPhi" ,&t_jetHJetGenJetPhi );
+        //tree->Branch("t_jetHJetGenJetEta" ,&t_jetHJetGenJetEta );
+        //tree->Branch("t_jetHJetGenJetE"   ,&t_jetHJetGenJetE );
+
+        tree->Branch("t_jetTopJetPt"            ,&t_jetTopJetPt );
+        tree->Branch("t_jetTopJetPhi"           ,&t_jetTopJetPhi );
+        tree->Branch("t_jetTopJetEta"           ,&t_jetTopJetEta );
+        tree->Branch("t_jetTopJetE"             ,&t_jetTopJetE );
+        tree->Branch("t_jetTopJetCSV"           ,&t_jetTopJetCSV );
+        tree->Branch("t_jetTopJetMass"          ,&t_jetTopJetMass );
+        tree->Branch("t_jetTopJetHadronFlavour" ,&t_jetTopJetHadronFlavour );
+        tree->Branch("t_jetTopJetPartonFlavour" ,&t_jetTopJetPartonFlavour );
+        tree->Branch("t_jetTopJet_tau1"         ,&t_jetTopJet_tau1        );
+        tree->Branch("t_jetTopJet_tau2"         ,&t_jetTopJet_tau2        );
+        tree->Branch("t_jetTopJet_tau3"         ,&t_jetTopJet_tau3        );
+        tree->Branch("t_jetTopJet_MassPruned"   ,&t_jetTopJet_MassPruned  );
+        tree->Branch("t_jetTopJet_SoftDropMass" ,&t_jetTopJet_SoftDropMass);
+        tree->Branch("t_jetTopJet_NSubJets"     ,&t_jetTopJet_NSubJets    );
+
+        //tree->Branch("t_jetTopJetGenJetPt"  ,&t_jetTopJetGenJetPt );
+        //tree->Branch("t_jetTopJetGenJetPhi" ,&t_jetTopJetGenJetPhi );
+        //tree->Branch("t_jetTopJetGenJetEta" ,&t_jetTopJetGenJetEta );
+        //tree->Branch("t_jetTopJetGenJetE"   ,&t_jetTopJetGenJetE );
+
+        tree->Branch("t_metPt"  ,&t_metPt );
+        tree->Branch("t_metPhi" ,&t_metPhi);
+        tree->Branch("t_metEta" ,&t_metEta);
+        tree->Branch("t_metE"   ,&t_metE  );
+
+        tree->Branch("t_HT", &t_HT, "t_HT/D") ; 
+        tree->Branch("t_ST", &t_ST, "t_ST/D") ; 
+
+        tree->Branch("t_evtwt"           ,&t_evtwt          ,"t_evtwt/D"          );
+        tree->Branch("t_presel_wt"       ,&t_presel_wt      ,"t_presel_wt/D"      );
+        tree->Branch("t_btagsf"          ,&t_btagsf         ,"t_btagsf/D"         );
+        tree->Branch("t_btagsf_bcUp"     ,&t_btagsf_bcUp    ,"t_btagsf_bcUp/D"    );
+        tree->Branch("t_btagsf_bcDown"   ,&t_btagsf_bcDown  ,"t_btagsf_bcDown/D"  );
+        tree->Branch("t_btagsf_lUp"      ,&t_btagsf_lUp     ,"t_btagsf_lUp/D"     );
+        tree->Branch("t_btagsf_lDown"    ,&t_btagsf_lDown   ,"t_btagsf_lDown/D"   );    
+        tree->Branch("t_sjbtagsf"        ,&t_sjbtagsf       ,"t_sjbtagsf/D"       );
+        tree->Branch("t_sjbtagsf_bcUp"   ,&t_sjbtagsf_bcUp  ,"t_sjbtagsf_bcUp/D"  );
+        tree->Branch("t_sjbtagsf_bcDown" ,&t_sjbtagsf_bcDown,"t_sjbtagsf_bcDown/D");
+        tree->Branch("t_sjbtagsf_lUp"    ,&t_sjbtagsf_lUp   ,"t_sjbtagsf_lUp/D"   );
+        tree->Branch("t_sjbtagsf_lDown"  ,&t_sjbtagsf_lDown ,"t_sjbtagsf_lDown/D" );    
 
         tree->Branch("t_genPartPt"         ,&t_genPartPt        );
         tree->Branch("t_genPartPhi"        ,&t_genPartPhi       );
@@ -327,11 +531,6 @@ namespace os2l {
         tree->Branch("t_genPartMom2Status" ,&t_genPartMom2Status);
         tree->Branch("t_genPartDau1Status" ,&t_genPartDau1Status);
         tree->Branch("t_genPartDau2Status" ,&t_genPartDau2Status);
-
-        tree->Branch("t_metPt"  ,&t_metPt );
-        tree->Branch("t_metPhi" ,&t_metPhi);
-        tree->Branch("t_metEta" ,&t_metEta);
-        tree->Branch("t_metE"   ,&t_metE  );
 
         return;
 
