6a1a36edbd62bba6215fc72b6e489f6291106af9
dalathmendis <rachitha123@gmail.com>
2017-02-21 13:45:14 -0600

    updating Lepton ID, Trig SF

--- interface/ApplyLeptonTrigSFs.h
+++ interface/ApplyLeptonTrigSFs.h
@@ -3,153 +3,62 @@
 
 class ApplyLeptonTrigSFs {
   public:
-
+   
     ~ApplyLeptonTrigSFs () {} 
     double TrigSFMu1(double pt, double eta){
       double sf_=0.0; 
-      if(pt > 500.) pt = 499.9;
-      if(pt > 10 && pt <= 20){     
-        if     (eta <= -2.1     && eta > -2.4)    sf_ = 0.470;
-        else if(eta <= -1.6     && eta > -2.1)    sf_ = 0.565;
-        else if(eta <= -1.2     && eta > -1.6)    sf_ = 0.603;
-        else if(eta <= -0.8     && eta > -1.2)    sf_ = 0.591;
-        else if(eta <= 0.0      && eta > -0.8)    sf_ = 0.612;
-        else if(eta <= 0.8      && eta > 0.0)     sf_ = 0.612;
-        else if(eta <= 1.2      && eta > 0.8)     sf_ = 0.591;
-        else if(eta <= 1.6      && eta > 1.2)     sf_ = 0.603;
-        else if(eta <= 2.1      && eta > 1.6)     sf_ = 0.565;
-        else if(eta < 2.4       && eta > 2.1)     sf_ = 0.470 ;
+      if(pt > 500.) pt = 500.0;
+      if(pt > 26 && pt <= 30){
+	if (abs(eta) <= 2.4 && abs(eta) > 2.1)       sf_ = 0.906216;
+        else if (abs(eta) <= 2.1 && abs(eta) > 1.2)  sf_ = 0.982503;
+        else if (abs(eta) <= 1.2 && abs(eta) > 0.9)  sf_ = 0.956337;
+        else if (abs(eta) <= 0.9 && abs(eta) > 0.0)  sf_ = 0.980357;
       }
-      else if(pt > 20 && pt <= 25){
-        if     (eta <= -2.1     && eta > -2.4)    sf_ = 0.778;
-        else if(eta <= -1.6     && eta > -2.1)    sf_ = 0.902;
-        else if(eta <= -1.2     && eta > -2.1)    sf_ = 0.947;
-        else if(eta <= -0.8     && eta > -1.2)    sf_ = 0.914;
-        else if(eta <= 0.0      && eta > -0.8)    sf_ = 0.930;
-        else if(eta <= 0.8      && eta > 0.0)     sf_ = 0.930;
-        else if(eta <= 1.2      && eta > 0.8)     sf_ = 0.914;
-        else if(eta <= 1.6      && eta > 1.2)     sf_ = 0.947;
-        else if(eta <= 2.1      && eta > 1.6)     sf_ = 0.902;
-        else if(eta < 2.4       && eta > 2.1)     sf_ = 0.778 ;          
-      }
-      else if(pt > 25 && pt <= 30){
-        if     (eta <= -2.1     && eta > -2.4)    sf_ = 0.817;
-        else if(eta <= -1.6     && eta > -2.1)    sf_ = 0.920;
-        else if(eta <= -1.2     && eta > -2.1)    sf_ = 0.956;
-        else if(eta <= -0.8     && eta > -1.2)    sf_ = 0.917;
-        else if(eta <= 0.0      && eta > -0.8)    sf_ = 0.931;
-        else if(eta <= 0.8      && eta > 0.0)     sf_ = 0.931;
-        else if(eta <= 1.2      && eta > 0.8)     sf_ = 0.917;
-        else if(eta <= 1.6      && eta > 1.2)     sf_ = 0.956;
-        else if(eta <= 2.1      && eta > 1.6)     sf_ = 0.920;
-        else if(eta < 2.4       && eta > 2.1)     sf_ = 0.817;
-      }
-
-      else if(pt > 30 && pt <= 35){
-        if     (eta <= -2.1     && eta > -2.4)    sf_ = 0.842;
-        else if(eta <= -1.6     && eta > -2.1)    sf_ = 0.928;
-        else if(eta <= -1.2     && eta > -2.1)    sf_ = 0.960;
-        else if(eta <= -0.8     && eta > -1.2)    sf_ = 0.916;
-        else if(eta <= 0.0      && eta > -0.8)    sf_ = 0.932;
-        else if(eta <= 0.8      && eta > 0.0)     sf_ = 0.932;
-        else if(eta <= 1.2      && eta > 0.8)     sf_ = 0.916;
-        else if(eta <= 1.6      && eta > 1.2)     sf_ = 0.960;
-        else if(eta <= 2.1      && eta > 1.6)     sf_ = 0.928;
-        else if(eta < 2.4       && eta > 2.1)     sf_ = 0.842;
-      }
-      else if(pt > 35 && pt <= 40){
-        if     (eta <= -2.1     && eta > -2.4)    sf_ = 0.858;
-        else if(eta <= -1.6     && eta > -2.1)    sf_ = 0.934;
-        else if(eta <= -1.2     && eta > -2.1)    sf_ = 0.962;
-        else if(eta <= -0.8     && eta > -1.2)    sf_ = 0.916;
-        else if(eta <= 0.0      && eta > -0.8)    sf_ = 0.932;
-        else if(eta <= 0.8      && eta > 0.0)     sf_ = 0.932;
-        else if(eta <= 1.2      && eta > 0.8)     sf_ = 0.916;
-        else if(eta <= 1.6      && eta > 1.2)     sf_ = 0.962;
-        else if(eta <= 2.1      && eta > 1.6)     sf_ = 0.934;
-        else if(eta < 2.4       && eta > 2.1)     sf_ = 0.858;
-      }
-
+      
+      else if(pt > 30 && pt <= 40){
+	if (abs(eta) <= 2.4 && abs(eta) > 2.1)       sf_ = 0.944555;
+        else if (abs(eta) <= 2.1 && abs(eta) > 1.2)  sf_ = 0.99548;
+        else if (abs(eta) <= 1.2 && abs(eta) > 0.9)  sf_ = 0.965959;
+        else if (abs(eta) <= 0.9 && abs(eta) > 0.0)  sf_ = 0.984096;
+     }
+    
       else if(pt > 40 && pt <= 50){
-        if     (eta <= -2.1     && eta > -2.4)    sf_ = 0.876 ;
-        else if(eta <= -1.6     && eta > -2.1)    sf_ = 0.936;
-        else if(eta <= -1.2     && eta > -2.1)    sf_ = 0.961;
-        else if(eta <= -0.8     && eta > -1.2)    sf_ = 0.917;
-        else if(eta <= 0.0      && eta > -0.8)    sf_ = 0.932;
-        else if(eta <= 0.8      && eta > 0.0)     sf_ = 0.932;
-        else if(eta <= 1.2      && eta > 0.8)     sf_ = 0.917;
-        else if(eta <= 1.6      && eta > 1.2)     sf_ = 0.961;
-        else if(eta <= 2.1      && eta > 1.6)     sf_ = 0.936;
-        else if(eta < 2.4       && eta > 2.1)     sf_ = 0.876;
+	if (abs(eta) <= 2.4 && abs(eta) > 2.1)       sf_ = 0.957532;
+        else if (abs(eta) <= 2.1 && abs(eta) > 1.2)  sf_ = 0.99912;
+        else if (abs(eta) <= 1.2 && abs(eta) > 0.9)  sf_ = 0.967858;
+        else if (abs(eta) <= 0.9 && abs(eta) > 0.0)  sf_ = 0.984904;
       }
 
-      else if(pt > 50 && pt <= 75){
-        if     (eta <= -2.1     && eta > -2.4)    sf_ = 0.886;
-        else if(eta <= -1.6     && eta > -2.1)    sf_ = 0.936;
-        else if(eta <= -1.2     && eta > -2.1)    sf_ = 0.956;
-        else if(eta <= -0.8     && eta > -1.2)    sf_ = 0.914;
-        else if(eta <= 0.0      && eta > -0.8)    sf_ = 0.929;
-        else if(eta <= 0.8      && eta > 0.0)     sf_ = 0.929;
-        else if(eta <= 1.2      && eta > 0.8)     sf_ = 0.914;
-        else if(eta <= 1.6      && eta > 1.2)     sf_ = 0.956;
-        else if(eta <= 2.1      && eta > 1.6)     sf_ = 0.936;
-        else if(eta < 2.4       && eta > 2.1)     sf_ = 0.886;
+      else if(pt > 50 && pt <= 60){
+	if (abs(eta) <= 2.4 && abs(eta) > 2.1)       sf_ = 0.960397;
+        else if (abs(eta) <= 2.1 && abs(eta) > 1.2)  sf_ = 0.99912;
+        else if (abs(eta) <= 1.2 && abs(eta) > 0.9)  sf_ = 0.968175;
+        else if (abs(eta) <= 0.9 && abs(eta) > 0.0)  sf_ = 0.985122;
       }
 
-      else if(pt > 75 && pt <= 100){
-        if     (eta <= -2.1     && eta > -2.4)    sf_ = 0.886;
-        else if(eta <= -1.6     && eta > -2.1)    sf_ = 0.928;
-        else if(eta <= -1.2     && eta > -2.1)    sf_ = 0.936;
-        else if(eta <= -0.8     && eta > -1.2)    sf_ = 0.911;
-        else if(eta <= 0.0      && eta > -0.8)    sf_ = 0.923;
-        else if(eta <= 0.8      && eta > 0.0)     sf_ = 0.923;
-        else if(eta <= 1.2      && eta > 0.8)     sf_ = 0.911;
-        else if(eta <= 1.6      && eta > 1.2)     sf_ = 0.936;
-        else if(eta <= 2.1      && eta > 1.6)     sf_ = 0.928;
-        else if(eta < 2.4       && eta > 2.1)     sf_ = 0.886;
+      else if(pt > 60 && pt <= 120){
+	if (abs(eta) <= 2.4 && abs(eta) > 2.1)       sf_ = 0.95241;
+        else if (abs(eta) <= 2.1 && abs(eta) > 1.2)  sf_ = 0.998937;
+        else if (abs(eta) <= 1.2 && abs(eta) > 0.9)  sf_ = 0.964898;
+        else if (abs(eta) <= 0.9 && abs(eta) > 0.0)  sf_ = 0.983897;
       }
 
-      else if(pt > 100 && pt <= 200){
-        if     (eta <= -2.1     && eta > -2.4)    sf_ = 0.872;
-        else if(eta <= -1.6     && eta > -2.1)    sf_ = 0.896;
-        else if(eta <= -1.2     && eta > -2.1)    sf_ = 0.898;
-        else if(eta <= -0.8     && eta > -1.2)    sf_ = 0.903;
-        else if(eta <= 0.0      && eta > -0.8)    sf_ = 0.920;
-        else if(eta <= 0.8      && eta > 0.0)     sf_ = 0.920;
-        else if(eta <= 1.2      && eta > 0.8)     sf_ = 0.903;
-        else if(eta <= 1.6      && eta > 1.2)     sf_ = 0.898;
-        else if(eta <= 2.1      && eta > 1.6)     sf_ = 0.896;
-        else if(eta < 2.4       && eta > 2.1)     sf_ = 0.872;
+      else if(pt > 120 && pt <= 200){
+	if (abs(eta) <= 2.4 && abs(eta) > 2.1)       sf_ = 0.977009;
+        else if (abs(eta) <= 2.1 && abs(eta) > 1.2)  sf_ = 1.00552;
+        else if (abs(eta) <= 1.2 && abs(eta) > 0.9)  sf_ = 0.946724;
+        else if (abs(eta) <= 0.9 && abs(eta) > 0.0)  sf_ = 0.976031;
       }
 
-      else if(pt > 200 && pt <= 300){
-        if     (eta <= -2.1     && eta > -2.4)    sf_ = 0.837;
-        else if(eta <= -1.6     && eta > -2.1)    sf_ = 0.786;
-        else if(eta <= -1.2     && eta > -2.1)    sf_ = 0.809;
-        else if(eta <= -0.8     && eta > -1.2)    sf_ = 0.871;
-        else if(eta <= 0.0      && eta > -0.8)    sf_ = 0.901;
-        else if(eta <= 0.8      && eta > 0.0)     sf_ = 0.901;
-        else if(eta <= 1.2      && eta > 0.8)     sf_ = 0.871;
-        else if(eta <= 1.6      && eta > 1.2)     sf_ = 0.809;
-        else if(eta <= 2.1      && eta > 1.6)     sf_ = 0.786;
-        else if(eta < 2.4       && eta > 2.1)     sf_ = 0.837;
-      }
-
-      else if(pt > 300 && pt <500){
-        if     (eta <= -2.1     && eta > -2.4)    sf_ = 0.700;
-        else if(eta <= -1.6     && eta > -2.1)    sf_ = 0.703;
-        else if(eta <= -1.2     && eta > -2.1)    sf_ = 0.718;
-        else if(eta <= -0.8     && eta > -1.2)    sf_ = 0.901;
-        else if(eta <= 0.0      && eta > -0.8)    sf_ = 0.899;
-        else if(eta <= 0.8      && eta > 0.0)     sf_ = 0.899;
-        else if(eta <= 1.2      && eta > 0.8)     sf_ = 0.901;
-        else if(eta <= 1.6      && eta > 1.2)     sf_ = 0.718;
-        else if(eta <= 2.1      && eta > 1.6)     sf_ = 0.703;
-        else if(eta < 2.4       && eta > 2.1)     sf_ = 0.700;
+      else if(pt > 200 && pt <= 500){
+	if (abs(eta) <= 2.4 && abs(eta) > 2.1)       sf_ = 0.918103;
+        else if (abs(eta) <= 2.1 && abs(eta) > 1.2)  sf_ = 0.982804;
+        else if (abs(eta) <= 1.2 && abs(eta) > 0.9)  sf_ = 0.949605;
+        else if (abs(eta) <= 0.9 && abs(eta) > 0.0)  sf_ = 0.983284;
       }
       return sf_ ;
     }
-
+    /*
     double TrigSFMu2(double pt, double eta){
       double sf_=0.0;
 
@@ -287,22 +196,24 @@ class ApplyLeptonTrigSFs {
         else if(eta <= 2.1      && eta > 1.6)     sf_ = 0.714;
         else if(eta < 2.4       && eta > 2.1)     sf_ = 0.800;
       }
-      /*
-         else if(pt > 20 && pt <= 25){
-         if     (eta <= -2.1     && eta > -2.4)    sf_ = ;
-         else if(eta <= -1.6     && eta > -2.1)    sf_ = ;
-         else if(eta <= -1.2     && eta > -2.1)    sf_ = ;
-         else if(eta <= -0.8     && eta > -1.2)    sf_ = ;
-         else if(eta <= 0.0      && eta > -0.8)    sf_ = ;
-         else if(eta <= 0.8      && eta > 0.0)     sf_ = ;
-         else if(eta <= 1.2      && eta > 0.8)     sf_ = ;
-         else if(eta <= 1.6      && eta > 1.2)     sf_ = ;
-         else if(eta <= 2.1      && eta > 1.6)     sf_ = ;
-         else if(eta < 2.4       && eta > 2.1)     sf_ = ;
-         }
-         */
-      return sf_ ; 
-    }
+      
+      else if(pt > 20 && pt <= 25){
+        if     (eta <= -2.1     && eta > -2.4)    sf_ = ;
+        else if(eta <= -1.6     && eta > -2.1)    sf_ = ;
+        else if(eta <= -1.2     && eta > -2.1)    sf_ = ;
+        else if(eta <= -0.8     && eta > -1.2)    sf_ = ;
+        else if(eta <= 0.0      && eta > -0.8)    sf_ = ;
+        else if(eta <= 0.8      && eta > 0.0)     sf_ = ;
+        else if(eta <= 1.2      && eta > 0.8)     sf_ = ;
+        else if(eta <= 1.6      && eta > 1.2)     sf_ = ;
+        else if(eta <= 2.1      && eta > 1.6)     sf_ = ;
+        else if(eta < 2.4       && eta > 2.1)     sf_ = ;
+      }
+      
+       return sf_ ; 
+       
+       }
+    */
   private: 
     double sf_ ;
     // double TrigSFMu1(double pt, double eta);
@@ -310,6 +221,6 @@ class ApplyLeptonTrigSFs {
     //    TrigSFEl1(double pt, double eta);
     //TrigSFEL2(double pt, double eta);
 
-
+   
 };
 #endif 
